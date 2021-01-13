26789322a65fdb1f8a99e742d1badced5fb5d032
anamariamr <63805557+anamariamr@users.noreply.github.com>
2021-01-13 14:30:04 -0600

    Lista la vista de los Abogados

--- plataforma_web/blueprints/abogados/models.py
+++ plataforma_web/blueprints/abogados/models.py
@@ -15,8 +15,11 @@ class Abogado(db.Model, UniversalMixin):
     id = db.Column(db.Integer, primary_key=True)
 
     # Columnas
-    nombre = db.Column(db.String(256), unique=True, nullable=False)
-    descripcion = db.Column(db.String(256), nullable=False)
+    numero = db.Column(db.Integer(), unique=True,  nullable=False)
+    nombre = db.Column(db.String(256), nullable=False)
+    libro = db.Column(db.String(256), nullable=False)
+    fecha = db.Column(db.DateTime, nullable=False)
+
 
     def __repr__(self):
         """ Representación """
--- plataforma_web/blueprints/abogados/views.py
+++ plataforma_web/blueprints/abogados/views.py
@@ -6,3 +6,26 @@ from flask_login import login_required
 from plataforma_web.blueprints.roles.models import Permiso
 from plataforma_web.blueprints.usuarios.decorators import permission_required
 
+from plataforma_web.blueprints.abogados.models import Abogado
+
+abogados = Blueprint('abogados', __name__, template_folder='templates')
+
+@abogados.before_request
+@login_required
+@permission_required(Permiso.VER_CATALOGOS)
+def before_request():
+    pass
+
+
+@abogados.route('/abogados')
+def list_active():
+    """ Listado de Abogados """
+    abogados_activos = Abogado.query.filter(Abogado.estatus == 'A').all()
+    return render_template('abogados/list.jinja2', abogados=abogados_activos)
+
+
+@abogados.route('/abogados/<int:abogado_id>')
+def detail(abogado_id):
+    """ Detalle de un Abogado """
+    abogado = Abogado.query.get_or_404(abogado_id)
+    return render_template('abogados/detail.jinja2', abogado=abogado)
