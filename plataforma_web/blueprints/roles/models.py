1f60d795ba81fafafb88eb8fbff0e2d26c575810
Guillermo Valdés <guillermo.valdes@pjecz.gob.mx>
2021-01-20 13:40:23 -0600

    Tomen los nuevos roles.

--- plataforma_web/blueprints/roles/models.py
+++ plataforma_web/blueprints/roles/models.py
@@ -101,7 +101,7 @@ class Rol(db.Model, UniversalMixin):
             return self.has_permission(Permiso.VER_CUENTAS)
         if module in ('distritos', 'autoridades'):
             return self.has_permission(Permiso.VER_CATALOGOS)
-        if module in ('abogados', 'glosas', 'listas_de_acuerdos', 'peritos'):
+        if module in ('abogados', 'edictos', 'glosas', 'listas_de_acuerdos', 'peritos', 'sentencias', 'ubicaciones_expedientes'):
             return self.has_permission(Permiso.VER_CONTENIDOS)
         return False
 
@@ -111,7 +111,7 @@ class Rol(db.Model, UniversalMixin):
             return self.has_permission(Permiso.MODIFICAR_CUENTAS)
         if module in ('distritos', 'autoridades'):
             return self.has_permission(Permiso.MODIFICAR_CATALOGOS)
-        if module in ('abogados', 'glosas', 'listas_de_acuerdos', 'peritos'):
+        if module in ('abogados', 'edictos', 'glosas', 'listas_de_acuerdos', 'peritos', 'sentencias', 'ubicaciones_expedientes'):
             return self.has_permission(Permiso.MODIFICAR_CONTENIDOS)
         return False
 
@@ -121,7 +121,7 @@ class Rol(db.Model, UniversalMixin):
             return self.has_permission(Permiso.CREAR_CUENTAS)
         if module in ('distritos', 'autoridades'):
             return self.has_permission(Permiso.CREAR_CATALOGOS)
-        if module in ('abogados', 'glosas', 'listas_de_acuerdos', 'peritos'):
+        if module in ('abogados', 'edictos', 'glosas', 'listas_de_acuerdos', 'peritos', 'sentencias', 'ubicaciones_expedientes'):
             return self.has_permission(Permiso.CREAR_CONTENIDOS)
         return False
 
