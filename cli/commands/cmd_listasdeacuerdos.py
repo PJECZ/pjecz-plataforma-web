6840f4d9c6cff9feb6a3621f99febb5a7612b38d
Guillermo Valdés <guillermo@movimientolibre.com>
2021-01-21 21:55:39 -0600

    Falta corregir los CSV de distritos y autoridades.

--- cli/commands/cmd_listasdeacuerdos.py
+++ cli/commands/cmd_listasdeacuerdos.py
@@ -4,16 +4,22 @@ Listas de Acuerdos
 #from pathlib import Path
 #import csv
 import click
-#from google.cloud import storage
+from google.cloud import storage
 
 from plataforma_web.app import create_app
+from plataforma_web.extensions import db
+
 from plataforma_web.blueprints.autoridades.models import Autoridad
+from plataforma_web.blueprints.distritos.models import Distrito
 from plataforma_web.blueprints.glosas.models import Glosa
 from plataforma_web.blueprints.listas_de_acuerdos.models import ListaDeAcuerdo
+from plataforma_web.blueprints.peritos.models import Perito
+
+app = create_app()
+db.app = app
 
 DEPOSITO = 'pjecz-consultas'
 DIRECTORIO = 'Listas de Acuerdos'
-app = create_app()
 
 
 @click.group()
@@ -24,11 +30,20 @@ def cli():
 @click.command()
 def alimentar():
     """ Alimentar """
-    #client = storage.Client()
-    #bucket = client.get_bucket(DEPOSITO)
+    # Google Cloud Storage
+    client = storage.Client()
+    bucket = client.get_bucket(DEPOSITO)
+    # Bucle por las autoridades
     autoridades_activas = Autoridad.query.filter(Autoridad.estatus == 'A').all()
-    #for autoridad in autoridades_activas:
-    #    click.echo(f'{DIRECTORIO}/{autoridad.distrito.nombre}/{autoridad.descripcion}')
+    for autoridad in autoridades_activas:
+        subdirectorio = f'{DIRECTORIO}/{autoridad.distrito.nombre}/{autoridad.descripcion}'
+        click.echo(subdirectorio)
+        #for blob in bucket.list_blobs(prefix=subdirectorio):
+        #    click.echo('  ' + blob.name)
+    # Real 'Distrito de Monclova', mal 'Distrito Judicial de Monclova'
+    subdirectorio = f'{DIRECTORIO}/Distrito de Monclova/Juzgado Primero de Primera Instancia en Materia Civil Monclova'
+    for blob in bucket.list_blobs(prefix=subdirectorio):
+        click.echo(f'  {blob.name}')
 
 
 cli.add_command(alimentar)
