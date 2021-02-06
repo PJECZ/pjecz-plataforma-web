560ed968bf0f1d92e225ee1b21e8e9aa402532fb
Guillermo Valdés <guillermo@movimientolibre.com>
2021-02-06 16:08:56 -0600

    Desactiva Firebase Auth si NO hay variables.

--- lib/firebase_auth.py
+++ lib/firebase_auth.py
@@ -5,12 +5,12 @@ import os
 
 
 firebase_auth = {
-    "apiKey": os.environ.get("FIREBASE_APIKEY"),
-    "authDomain": os.environ.get("FIREBASE_AUTHDOMAIN"),
-    "databaseURL": os.environ.get("FIREBASE_DATABASEURL"),
-    "projectId": os.environ.get("FIREBASE_PROJECTID"),
-    "storageBucket": os.environ.get("FIREBASE_STORAGEBUCKET"),
-    "messagingSenderId": os.environ.get("FIREBASE_MESSAGINGSENDERID"),
-    "appId": os.environ.get("FIREBASE_APPID"),
-    "measurementId": os.environ.get("FIREBASE_MEASUREMENTID"),
+    "apiKey": os.environ.get("FIREBASE_APIKEY", ""),
+    "authDomain": os.environ.get("FIREBASE_AUTHDOMAIN", ""),
+    "databaseURL": os.environ.get("FIREBASE_DATABASEURL", ""),
+    "projectId": os.environ.get("FIREBASE_PROJECTID", ""),
+    "storageBucket": os.environ.get("FIREBASE_STORAGEBUCKET", ""),
+    "messagingSenderId": os.environ.get("FIREBASE_MESSAGINGSENDERID", ""),
+    "appId": os.environ.get("FIREBASE_APPID", ""),
+    "measurementId": os.environ.get("FIREBASE_MEASUREMENTID", ""),
 }
