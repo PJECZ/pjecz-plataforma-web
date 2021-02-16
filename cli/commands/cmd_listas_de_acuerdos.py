cd8c3dfdc26db9cf3bf85dbc4a05906673d285a5
Guillermo Valdés <guillermo@movimientolibre.com>
2021-02-16 16:02:54 -0600

    Construir listas de acuerdos.

--- cli/commands/cmd_listas_de_acuerdos.py
+++ cli/commands/cmd_listas_de_acuerdos.py
@@ -2,15 +2,20 @@
 Listas de Acuerdos
 
 - alimentar: Alimentar la BD a partir de los archivos en Storage
+- construir: Construir la estructura de directorios y archivos para el constructor
 - respaldar: Respaldar la tabla 'listas_de_acuerdos' a su archivo CSV
 - publicar: Publicar el archivo JSON en Storage para que el sitio web lo use con DataTables
 """
 from datetime import datetime
 from pathlib import Path
-import json
 import csv
+import json
+import re
+import unicodedata
+
 import click
 from google.cloud import storage
+from jinja2 import Environment, FileSystemLoader
 
 from plataforma_web.app import create_app
 from plataforma_web.extensions import db
@@ -28,6 +33,17 @@ DEPOSITO = "pjecz-consultas"
 SUBDIRECTORIO = "Listas de Acuerdos"
 LISTAS_DE_ACUERDOS_CSV = "seed/listas_de_acuerdos.csv"
 LISTAS_DE_ACUERDOS_JSON = "json/listas_de_acuerdos.json"
+PELICAN_CONSULTAS = "/home/guivaloz/Documentos/GitHub/PJECZ/constructor-web-pjecz-gob-mx/content/consultas/listas-de-acuerdos"
+
+
+def cambiar_a_caracteres_seguros(texto):
+    """ Cambia caracteres acentuados a caracteres sin acento, cambia espacios por guiones """
+    texto = unicodedata.normalize("NFD", texto)
+    texto = texto.encode("ascii", "ignore")
+    texto = texto.decode("utf-8")
+    texto = re.sub("[ ]+", "-", texto)
+    texto = re.sub("[^0-9a-zA-Z_/-]", "", texto)
+    return texto.lower()
 
 
 @click.group()
@@ -80,6 +96,55 @@ def alimentar():
             if contador % 100 == 0:
                 click.echo(f"  Van {contador} registros...")
     click.echo(f"Alimentados {contador} registros.")
+
+
+@click.command()
+def construir():
+    """ Construir los directorios y archivos para listas de acuerdos """
+    # Preparar plantillas
+    plantillas_ruta = Path("cli/templates")
+    if not (plantillas_ruta.exists() or plantillas_ruta.is_dir()):
+        click.echo("No existe el directorio con las plantillas jinja2.")
+        return
+    plantillas_env = Environment(
+        loader=FileSystemLoader(str(plantillas_ruta)),
+        trim_blocks=True,
+        lstrip_blocks=True,
+    )
+    plantilla = plantillas_env.get_template("lista_de_acuerdo.jinja2")
+    # Verificar
+    pelican_consultas_ruta = Path(PELICAN_CONSULTAS)
+    if not pelican_consultas_ruta.exists():
+        click.echo("No existe la pelican_consultas_ruta a los contenidos del Pelican.")
+        return
+    # Bucle
+    contador = 0
+    autoridades = Autoridad.query.filter(Autoridad.estatus == "A").all()
+    for autoridad in autoridades:
+        if autoridad.directorio_listas_de_acuerdos == "":
+            continue
+        # Directorio
+        distrito_str = cambiar_a_caracteres_seguros(autoridad.distrito.nombre)
+        if distrito_str[:12] == "distrito-de-":
+            distrito_str = distrito_str[12:]
+        autoridad_str = cambiar_a_caracteres_seguros(autoridad.descripcion)
+        ruta = Path(pelican_consultas_ruta, distrito_str, autoridad_str)
+        ruta.mkdir(parents=True, exist_ok=True)
+        # Archivo md
+        archivo_ruta = Path(ruta, f"{autoridad_str}.md")
+        ingredientes = {}
+        ingredientes["title"] = f"Listas de Acuerdos {autoridad.descripcion}"
+        ingredientes["slug"] = cambiar_a_caracteres_seguros(f"consultas listas de acuerdos {autoridad.distrito.nombre} {autoridad.descripcion}")
+        ingredientes["summary"] = "Listas de Acuerdos."
+        ingredientes["url"] = ""
+        ingredientes["save_as"] = ""
+        ingredientes["content"] = "Lista de Acuerdos."
+        cocinado = plantilla.render(**ingredientes)
+        with open(archivo_ruta, "w") as puntero:
+            puntero.write(cocinado)
+        click.echo("- " + str(ruta))
+        contador += 1
+    click.echo(f"Construidos {contador} páginas en listas de acuerdos.")
 
 
 @click.command()
@@ -145,5 +210,6 @@ def publicar():
 
 
 cli.add_command(alimentar)
+cli.add_command(construir)
 cli.add_command(respaldar)
 cli.add_command(publicar)
