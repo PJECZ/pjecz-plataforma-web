670df6291cf3e1947afb1b0f888959217fac478c
anamariamr <63805557+anamariamr@users.noreply.github.com>
2021-02-19 13:10:54 -0600

    se agregan tipos de peritos

--- plataforma_web/blueprints/peritos/models.py
+++ plataforma_web/blueprints/peritos/models.py
@@ -12,9 +12,11 @@ class Perito(db.Model, UniversalMixin):
     TIPOS = OrderedDict([
         ('ALBACEA', 'Albacea'),
         ('AMBIENTAL', 'Ambiental'),
+        ('ARBITRO', 'Árbitro'),
         ('ARBITRO VOLUNTARIO', 'Árbitro Voluntario'),
         ('AUDITORIA INTERNA', 'Auditoría Interna'),
         ('BALISTICA', 'Balística'),
+        ('CALIGRAFIA', 'Caligrafía'),
         ('CONTABILIDAD', 'Contabilidad'),
         ('CRIMINOLOGIA', 'Criminología'),
         ('CRIMINALISTICA', 'Criminalística'),
@@ -24,21 +26,28 @@ class Perito(db.Model, UniversalMixin):
         ('FOTOGRAFIA FORENSE', 'Fotografía Forence'),
         ('GASTROENTEROLOGIA', 'Gastroenterología'),
         ('GENETICA', 'Genética'),
+        ('GENETICA HUMANA','Genética Humana')
+        ('GRAFOLOGIA','Grafología')
         ('GRAFOSCOPIA', 'Grafoscopía'),
         ('HECHOS DE TRANSITO TERRESTRE', 'Hechos de Tránsito Terrestre'),
         ('INCENDIOS Y EXPLOSIVOS', 'Incendios y Explosivos'),
+        ('INFORMATICA','Informática')
         ('INTERPRETE EN LENGUA DE SEÑAS MEXICANAS', 'Intérprete en Lengua de Señas Mexicanas'),
         ('INTERVENTORES', 'Interventores'),
         ('MEDICINA', 'Medicina'),
-        ('MEDICINA FORENCE', 'Medicina Forence'),
+        ('MEDICINA FORENSE', 'Medicina Forense'),
         ('ODONTOLOGIA LEGAL Y FORENCE', 'Odontología Legal y Forense'),
+        ('POLIGRAFIA','Poligrafía')
         ('PSICOLOGIA', 'Psicología'),
+        ('QUIMICA', 'Química'),
         ('TOPOGRAFIA', 'Topografía'),
         ('TRADUCCION', 'Traducción'),
         ('TRANSITO Y VIALIDAD TERRESTRE', 'Tránsito y Vialidad Terrestre'),
         ('TUTORES', 'Tutores'),
         ('VALUACION', 'Valuación'),
+        ('VALUACION INMOBILIARIA','Valuación Inmobiliaria')
         ('VALUACION VEHICULOS DAÑADOS', 'Valuación Vehículos Dañados'),
+        ('VALUACION AGROPECUARIA MAQUINARIA AGRICOLA Y EQUIPO INDUSTRIAL','Valuación agropecuaria maquinaria agrícola y equipo industrial')
     ])
 
     # Nombre de la tabla
