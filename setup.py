7add9ae1721dea045a283b77864f930efe1cc053
Guillermo Valdés <guillermo@movimientolibre.com>
2021-03-08 05:41:58 -0600

    Mejor requirements.txt

--- setup.py
+++ setup.py
@@ -1,33 +1,12 @@
 """
 Setup sirve para installar los comandos click
 """
+from setuptools import setup
 
-import setuptools
 
-setuptools.setup(
+setup(
     name="plataforma_web",
     version="0.1",
-    install_requires=[
-        "click",
-        "cryptography",
-        "email-validator",
-        "Flask",
-        "Flask-Login",
-        "Flask-SQLAlchemy",
-        "Flask-WTF",
-        "google-api-python-client",
-        "google-auth",
-        "google-auth-httplib2",
-        "google-cloud-storage",
-        "Jinja2",
-        "passlib",
-        "psycopg2-binary",
-        "PyMySQL",
-        "SQLAlchemy",
-        "SQLAlchemy-utils",
-        "tabulate",
-        "unidecode",
-    ],
     entry_points="""
         [console_scripts]
         plataforma_web=cli.cli:cli
