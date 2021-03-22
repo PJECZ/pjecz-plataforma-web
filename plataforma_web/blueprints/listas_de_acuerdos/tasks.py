b8b9c224a6cf9c6a08676a86a313c075a4ca36df
Guillermo Valdés <guillermo@movimientolibre.com>
2021-03-22 07:05:41 -0600

    Avance lsitas de acuerdos tarea agregar con email.

--- plataforma_web/blueprints/listas_de_acuerdos/tasks.py
+++ plataforma_web/blueprints/listas_de_acuerdos/tasks.py
@@ -2,7 +2,7 @@
 Listas de Acuerdos, tareas para ejecutar en el fondo
 """
 import logging
-from datetime import datetime
+from datetime import datetime, date
 from pathlib import Path
 
 import google.cloud.storage
@@ -27,17 +27,94 @@ DEPOSITO = "pjecz-consultas"
 SUBDIRECTORIO = "Listas de Acuerdos"
 
 
-def set_task_progress(progress: int):
+def set_task_progress(progress: int, mensaje: str = None):
     """ Cambiar el progreso de la tarea """
-    bitacora.info("- Progreso %d %%", progress)
     job = get_current_job()
     if job:
         job.meta["progress"] = progress
         job.save_meta()
         tarea = Tarea.query.get(job.get_id())
-        if tarea and progress >= 100:
-            tarea.ha_terminado = True
+        if tarea:
+            if progress >= 100:
+                tarea.ha_terminado = True
+            if mensaje is not None:
+                tarea.descripcion = mensaje
             tarea.save()
+
+
+def set_task_error(mensaje: str):
+    """ Al fallar la tarea debe tomar el mensaje y terminarla """
+    job = get_current_job()
+    if job:
+        job.meta["progress"] = 100
+        job.save_meta()
+        tarea = Tarea.query.get(job.get_id())
+        if tarea:
+            tarea.ha_terminado = True
+            tarea.descripcion = mensaje
+            tarea.save()
+
+
+def agregar_con_correo_electronico(autoridad_email, fecha, archivo, descripcion=None, url=None):
+    """ Construir la estructura de directorios y archivos para Pelican """
+    # Validar autoridad_email
+    autoridad = Autoridad.query.filter(Autoridad.email == autoridad_email).first()
+    if autoridad is False:
+        mensaje = f"El e-mail {autoridad_email} no se encuentra en autoridades"
+        bitacora.error(mensaje)
+        set_task_error(mensaje)
+        return mensaje
+    # Validar fecha
+    if not isinstance(fecha, date):
+        try:
+            fecha = datetime.strptime(fecha, "%Y-%m-%d")
+        except ValueError as mensaje:
+            mensaje = f"La fecha {str(fecha)} es incorrecta."
+            bitacora.error(mensaje)
+            set_task_error(mensaje)
+            return mensaje
+    hoy = datetime.date.today()
+    pasado = hoy - datetime.timedelta(days=5)
+    if fecha < pasado or fecha > hoy:
+        mensaje = f"La fecha {str(fecha)} está fuera del rango de cinco días."
+        bitacora.error(mensaje)
+        set_task_error(mensaje)
+        return mensaje
+    # Validar archivo
+    archivo = archivo.strip()
+    if archivo == "":
+        mensaje = "Está vacío el nombre de archivo."
+        bitacora.error(mensaje)
+        set_task_error(mensaje)
+        return mensaje
+    # Validar descripcion
+    if descripcion is None:
+        descripcion = "Lista de acuerdos"
+    else:
+        descripcion = descripcion.strip()
+    # Validar URL
+    if url is None:
+        pass  # Por hacer crear URL aquí mismo
+    # Si existe ese registro (autoridad, fecha) en la tabla
+    lista_de_acuerdo = ListaDeAcuerdo.query.filter(ListaDeAcuerdo.autoridad_id == autoridad.id).filter(ListaDeAcuerdo.fecha == fecha).first()
+    if lista_de_acuerdo:
+        # Actualizar
+        lista_de_acuerdo.archivo = archivo
+        lista_de_acuerdo.descripcion = descripcion
+        lista_de_acuerdo.url = url
+        mensaje = f"Actualizada la lista de acuerdos del {fecha}"
+    else:
+        # Insertar
+        lista_de_acuerdo = ListaDeAcuerdo(
+            autoridad=autoridad,
+            fecha=fecha,
+            descripcion=descripcion,
+            url=url,
+        )
+        mensaje = f"Nueva lista de acuerdos del {fecha}"
+    bitacora.info(mensaje)
+    set_task_progress(100, mensaje=mensaje)
+    return mensaje
 
 
 def construir():
