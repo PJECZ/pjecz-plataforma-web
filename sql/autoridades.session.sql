b5f48a135563850ce1d13410ab640cd9508af89f
Guillermo Valdés <guillermo.valdes@pjecz.gob.mx>
2021-06-15 11:12:56 -0500

    SQL Sessions.

--- sql/autoridades.session.sql
+++ sql/autoridades.session.sql
@@ -1,36 +1,59 @@
--- @block jurisdiccionales activas
+-- @block autoridades jurisdiccionales activas
 SELECT autoridades.clave,
-    distritos.nombre,
+    distritos.nombre AS distrito,
     autoridades.descripcion
 FROM autoridades
-    INNER JOIN distritos ON autoridades.distrito = distritos.id
+    INNER JOIN distritos ON autoridades.distrito_id = distritos.id
 WHERE autoridades.es_jurisdiccional = TRUE
     AND autoridades.estatus = 'A'
 ORDER BY autoridades.clave;
--- @block jurisdiccionales inactivas
+-- @block autoridades jurisdiccionales inactivas
 SELECT autoridades.clave,
-    distritos.nombre,
+    distritos.nombre AS distrito,
     autoridades.descripcion
 FROM autoridades
-    INNER JOIN distritos ON autoridades.distrito = distritos.id
+    INNER JOIN distritos ON autoridades.distrito_id = distritos.id
 WHERE autoridades.es_jurisdiccional = TRUE
     AND autoridades.estatus = 'B'
 ORDER BY autoridades.clave;
--- @block no jusrisdiccionales activas
+-- @block autoridades no jusrisdiccionales activas
 SELECT autoridades.clave,
-    distritos.nombre,
+    distritos.nombre AS distrito,
     autoridades.descripcion
 FROM autoridades
-    INNER JOIN distritos ON autoridades.distrito = distritos.id
+    INNER JOIN distritos ON autoridades.distrito_id = distritos.id
 WHERE autoridades.es_jurisdiccional = FALSE
     AND autoridades.estatus = 'A'
 ORDER BY autoridades.clave;
--- @block no jusrisdiccionales inactivas
+-- @block autoridades no jusrisdiccionales inactivas
 SELECT autoridades.clave,
-    distritos.nombre,
+    distritos.nombre AS distrito,
     autoridades.descripcion
 FROM autoridades
-    INNER JOIN distritos ON autoridades.distrito = distritos.id
+    INNER JOIN distritos ON autoridades.distrito_id = distritos.id
 WHERE autoridades.es_jurisdiccional = FALSE
     AND autoridades.estatus = 'B'
 ORDER BY autoridades.clave;
+-- @block organos_jurisdiccionales y autoridades
+SELECT autoridades.organo_jurisdiccional,
+    autoridades.clave,
+    autoridades.descripcion
+FROM autoridades
+WHERE autoridades.estatus = 'A'
+    AND autoridades.es_jurisdiccional = TRUE
+    AND autoridades.es_notaria = FALSE
+ORDER BY organo_jurisdiccional,
+    clave;
+-- @block organos_jurisdiccionales, materias y autoridades
+SELECT autoridades.organo_jurisdiccional,
+    materias.nombre AS materia,
+    autoridades.clave,
+    autoridades.descripcion
+FROM autoridades
+    INNER JOIN materias ON autoridades.materia_id = materias.id
+WHERE autoridades.estatus = 'A'
+    AND autoridades.es_jurisdiccional = TRUE
+    AND autoridades.es_notaria = FALSE
+ORDER BY organo_jurisdiccional,
+    materia,
+    clave
