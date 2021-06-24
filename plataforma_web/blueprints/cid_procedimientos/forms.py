be346a3f4baa58930a5d6b79d06d5d7c001738fb
Guillermo Valdés <guillermo.valdes@pjecz.gob.mx>
2021-06-24 11:33:44 -0500

    En Procedimientos funciona Quill.

--- plataforma_web/blueprints/cid_procedimientos/forms.py
+++ plataforma_web/blueprints/cid_procedimientos/forms.py
@@ -2,13 +2,13 @@
 CID Procedimientos, formularios
 """
 from flask_wtf import FlaskForm
-from wtforms import HiddenField, StringField, SubmitField
-from wtforms.validators import DataRequired, Length
+from wtforms import HiddenField, StringField, SubmitField, TextAreaField
+from wtforms.validators import DataRequired, Length, Optional
 
 
 class CIDProcedimientoForm(FlaskForm):
-    """ Formulario CID Procedimiento """
+    """Formulario CID Procedimiento"""
 
     descripcion = StringField("Descripción", validators=[DataRequired(), Length(max=256)])
-    contenido = HiddenField("Contenido", validators=[DataRequired()])
+    contenido = HiddenField("Contenido", validators=None)  # Por javascript se copia del Quill
     guardar = SubmitField("Guardar")
