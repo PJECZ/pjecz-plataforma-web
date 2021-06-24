4394b36c2309fa925607e719ce9983c99b90a658
Guillermo Valdés <guillermo.valdes@pjecz.gob.mx>
2021-06-24 13:08:01 -0500

    SID funciona editar.

--- plataforma_web/blueprints/cid_procedimientos/forms.py
+++ plataforma_web/blueprints/cid_procedimientos/forms.py
@@ -2,13 +2,13 @@
 CID Procedimientos, formularios
 """
 from flask_wtf import FlaskForm
-from wtforms import HiddenField, StringField, SubmitField, TextAreaField
-from wtforms.validators import DataRequired, Length, Optional
+from wtforms import HiddenField, StringField, SubmitField
+from wtforms.validators import DataRequired, Length
 
 
 class CIDProcedimientoForm(FlaskForm):
     """Formulario CID Procedimiento"""
 
-    descripcion = StringField("Descripción", validators=[DataRequired(), Length(max=256)])
+    descripcion = StringField("Descripción", validators=[DataRequired(), Length(max=128)])
     contenido = HiddenField("Contenido", validators=None)  # Por javascript se copia del Quill
     guardar = SubmitField("Guardar")
