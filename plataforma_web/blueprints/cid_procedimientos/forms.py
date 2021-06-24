f9cc45e39f334ce9838bb817402c1c15efaa6a32
Guillermo Valdés <guillermo.valdes@pjecz.gob.mx>
2021-06-23 22:19:25 -0500

    Prueba con Quill.

--- plataforma_web/blueprints/cid_procedimientos/forms.py
+++ plataforma_web/blueprints/cid_procedimientos/forms.py
@@ -2,7 +2,7 @@
 CID Procedimientos, formularios
 """
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import HiddenField, StringField, SubmitField
 from wtforms.validators import DataRequired, Length
 
 
@@ -10,4 +10,5 @@ class CIDProcedimientoForm(FlaskForm):
     """ Formulario CID Procedimiento """
 
     descripcion = StringField("Descripción", validators=[DataRequired(), Length(max=256)])
+    contenido = HiddenField("Contenido", validators=[DataRequired()])
     guardar = SubmitField("Guardar")
