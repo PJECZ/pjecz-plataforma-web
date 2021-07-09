8163ab646fd654063904c4e8e155c4ba93cffd4e
Guillermo Valdés <guillermo.valdes@pjecz.gob.mx>
2021-07-09 15:04:57 -0500

    Columnas de acuerdos a varchar vacio por defecto.

--- plataforma_web/blueprints/listas_de_acuerdos_acuerdos/models.py
+++ plataforma_web/blueprints/listas_de_acuerdos_acuerdos/models.py
@@ -19,12 +19,12 @@ class ListaDeAcuerdoAcuerdo(db.Model, UniversalMixin):
     lista_de_acuerdo = db.relationship("ListaDeAcuerdo", back_populates="listas_de_acuerdos_acuerdos")
 
     # Columnas
-    folio = db.Column(db.String(16))
-    expediente = db.Column(db.String(16))
-    actor = db.Column(db.String(256), nullable=False)
-    demandado = db.Column(db.String(256), nullable=False)
-    tipo_acuerdo = db.Column(db.String(256), nullable=False)
-    tipo_juicio = db.Column(db.String(256), nullable=False)
+    folio = db.Column(db.String(16), nullable=False, default="", server_default="")
+    expediente = db.Column(db.String(16), nullable=False, default="", server_default="")
+    actor = db.Column(db.String(256), nullable=False, default="", server_default="")
+    demandado = db.Column(db.String(256), nullable=False, default="", server_default="")
+    tipo_acuerdo = db.Column(db.String(256), nullable=False, default="", server_default="")
+    tipo_juicio = db.Column(db.String(256), nullable=False, default="", server_default="")
     referencia = db.Column(db.Integer(), nullable=False)
 
     def __repr__(self):
