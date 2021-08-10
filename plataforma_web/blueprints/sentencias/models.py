29ae13ba99ee4aac2a8ec688c3b02eaeb0981bbd
Guillermo Valdés <guillermo.valdes@pjecz.gob.mx>
2021-08-10 16:27:22 -0500

    Avance en nuevos campos de sentencias.

--- plataforma_web/blueprints/sentencias/models.py
+++ plataforma_web/blueprints/sentencias/models.py
@@ -23,27 +23,33 @@ class Sentencia(db.Model, UniversalMixin):
     # Clave primaria
     id = db.Column(db.Integer, primary_key=True)
 
-    # Claves foráneas
+    # Clave foránea autoridad
     autoridad_id = db.Column(db.Integer, db.ForeignKey("autoridades.id"), index=True, nullable=False)
     autoridad = db.relationship("Autoridad", back_populates="sentencias")
+
+    # Columnas
+    sentencia = db.Column(db.String(16), index=True, nullable=False)
+    sentencia_fecha = db.Column(db.Date, index=True, nullable=True)
+    expediente = db.Column(db.String(16), index=True, nullable=False)
+    es_paridad_genero = db.Column(db.Boolean, nullable=False, default=False)
+    fecha = db.Column(db.Date, index=True, nullable=False)
+
+    # NUEVO Clave foránea
     materia_id = db.Column(db.Integer, db.ForeignKey("materias.id"), index=True, nullable=False)
     materia = db.relationship("Materia", back_populates="sentencias")
 
-    # Columnas
-    fecha = db.Column(db.Date, index=True, nullable=False)
-    sentencia = db.Column(db.String(16), index=True, nullable=False)
-    expediente = db.Column(db.String(16), index=True, nullable=False)
-    es_paridad_genero = db.Column(db.Boolean, nullable=False, default=False)
-    archivo = db.Column(db.String(256))
-    url = db.Column(db.String(512))
+    # NUEVO Columnas
     descripcion = db.Column(db.String(256), nullable=False, default="", server_default="")
-    fecha_sentencia = db.Column(db.Date, index=True, nullable=True)
     tipo_juicio = db.Column(
         db.Enum(*TIPOS_JUICIOS, name="tipos_juicios", native_enum=False),
         index=True,
         nullable=False,
         server_default="ND",
     )
+
+    # Archivo
+    archivo = db.Column(db.String(256))
+    url = db.Column(db.String(512))
 
     def __repr__(self):
         """Representación"""
