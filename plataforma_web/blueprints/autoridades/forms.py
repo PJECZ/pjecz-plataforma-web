23c0ef559c25a40ab11ee40b230ba91c78f1918f
Guillermo Valdés <guillermo.valdes@pjecz.gob.mx>
2021-10-07 13:59:38 -0500

    En Autoridades todos los campos necesarios.

--- plataforma_web/blueprints/autoridades/forms.py
+++ plataforma_web/blueprints/autoridades/forms.py
@@ -2,11 +2,13 @@
 Autoridades, formularios
 """
 from flask_wtf import FlaskForm
-from wtforms import BooleanField, StringField, SubmitField
+from wtforms import BooleanField, IntegerField, SelectField, StringField, SubmitField
 from wtforms.ext.sqlalchemy.fields import QuerySelectField
-from wtforms.validators import DataRequired, Length, Optional
+from wtforms.validators import DataRequired, Length, NumberRange, Optional
 
+from plataforma_web.blueprints.autoridades.models import Autoridad
 from plataforma_web.blueprints.distritos.models import Distrito
+from plataforma_web.blueprints.materias.models import Materia
 
 
 def distritos_opciones():
@@ -14,12 +16,41 @@ def distritos_opciones():
     return Distrito.query.filter_by(estatus="A").order_by(Distrito.nombre).all()
 
 
-class AutoridadForm(FlaskForm):
-    """Formulario Autoridad"""
+def materias_opciones():
+    """Materias: opciones para select"""
+    return Materia.query.filter_by(estatus="A").order_by(Materia.nombre).all()
 
-    distrito = QuerySelectField(query_factory=distritos_opciones, get_label="nombre")
+
+class AutoridadNewForm(FlaskForm):
+    """Formulario nueva Autoridad"""
+
+    distrito = QuerySelectField("Departamento", query_factory=distritos_opciones, get_label="nombre", validators=[DataRequired()])
     descripcion = StringField("Descripción", validators=[DataRequired(), Length(max=256)])
-    descripcion_corta = StringField("Descripción corta", validators=[DataRequired(), Length(max=256)])
-    clave = StringField("Clave", validators=[DataRequired(), Length(max=256)])
+    descripcion_corta = StringField("Descripción corta (máximo 64 caracteres)", validators=[DataRequired(), Length(max=64)])
+    clave = StringField("Clave (única, máximo 16 caracteres)", validators=[DataRequired(), Length(max=16)])
     es_jurisdiccional = BooleanField("Es Jurisdiccional", validators=[Optional()])
+    es_notaria = BooleanField("Es Notaría", validators=[Optional()])
+    organo_jurisdiccional = SelectField("Órgano Jurisdiccional", choices=Autoridad.ORGANOS_JURISDICCIONALES, validators=[DataRequired()])
+    materia = QuerySelectField("Materia (si es de Primera Instancia)", query_factory=materias_opciones, get_label="nombre", validators=[DataRequired()])
+    audiencia_categoria = SelectField("Categoría de audiencias", choices=Autoridad.AUDIENCIAS_CATEGORIAS, validators=[DataRequired()])
+    guardar = SubmitField("Guardar")
+
+
+class AutoridadEditForm(FlaskForm):
+    """Formulario modificar Autoridad"""
+
+    distrito = QuerySelectField("Departamento", query_factory=distritos_opciones, get_label="nombre", validators=[DataRequired()])
+    descripcion = StringField("Descripción", validators=[DataRequired(), Length(max=256)])
+    descripcion_corta = StringField("Descripción corta (máximo 64 caracteres)", validators=[DataRequired(), Length(max=64)])
+    clave = StringField("Clave (única, máximo 16 caracteres)", validators=[DataRequired(), Length(max=16)])
+    es_jurisdiccional = BooleanField("Es Jurisdiccional", validators=[Optional()])
+    es_notaria = BooleanField("Es Notaría", validators=[Optional()])
+    organo_jurisdiccional = SelectField("Órgano Jurisdiccional", choices=Autoridad.ORGANOS_JURISDICCIONALES, validators=[DataRequired()])
+    materia = QuerySelectField("Materia (si es de Primera Instancia)", query_factory=materias_opciones, get_label="nombre", validators=[DataRequired()])
+    audiencia_categoria = SelectField("Categoría de audiencias", choices=Autoridad.AUDIENCIAS_CATEGORIAS, validators=[DataRequired()])
+    directorio_edictos = StringField("Directorio para edictos", validators=[Optional(), Length(max=256)])
+    directorio_glosas = StringField("Directorio para glosas", validators=[Optional(), Length(max=256)])
+    directorio_listas_de_acuerdos = StringField("Directorio para listas de acuerdos", validators=[Optional(), Length(max=256)])
+    directorio_sentencias = StringField("Directorio para sentencias", validators=[Optional(), Length(max=256)])
+    limite_dias_listas_de_acuerdos = IntegerField("Límite días para listas de acuerdos", validators=[NumberRange(0, 30)])
     guardar = SubmitField("Guardar")
--- plataforma_web/blueprints/autoridades/views.py
+++ plataforma_web/blueprints/autoridades/views.py
@@ -1,21 +1,37 @@
 """
 Autoridades, vistas
 """
-from flask import Blueprint, flash, render_template, redirect, url_for
+from datetime import date, datetime, timedelta
+from flask import Blueprint, flash, redirect, render_template, url_for
 from flask_login import current_user, login_required
+import pytz
 
 from lib.safe_string import safe_message
+from plataforma_web.blueprints.usuarios.decorators import permission_required
 
-from plataforma_web.blueprints.bitacoras.models import Bitacora
+from plataforma_web.blueprints.audiencias.models import Audiencia
 from plataforma_web.blueprints.autoridades.models import Autoridad
-from plataforma_web.blueprints.autoridades.forms import AutoridadForm
+from plataforma_web.blueprints.autoridades.forms import AutoridadEditForm, AutoridadNewForm
+from plataforma_web.blueprints.bitacoras.models import Bitacora
+from plataforma_web.blueprints.edictos.models import Edicto
 from plataforma_web.blueprints.modulos.models import Modulo
 from plataforma_web.blueprints.permisos.models import Permiso
-from plataforma_web.blueprints.usuarios.decorators import permission_required
+from plataforma_web.blueprints.listas_de_acuerdos.models import ListaDeAcuerdo
+from plataforma_web.blueprints.sentencias.models import Sentencia
 
 MODULO = "AUTORIDADES"
 
 autoridades = Blueprint("autoridades", __name__, template_folder="templates")
+
+HOY = date.today()
+BREVE_LIMITE_DIAS = 15
+BREVE_DESDE_DATETIME = datetime(year=HOY.year, month=HOY.month, day=HOY.day) + timedelta(days=-BREVE_LIMITE_DIAS)
+ZONA_HORARIA = pytz.timezone("America/Mexico_City")
+
+TARJETA_SATISFACTORIA = "bg-light"
+TARJETA_ADVERTENCIA = "bg-warning"
+TARJETA_VACIA = "bg-danger"
+TARJETAS_LIMITE_REGISTROS = 5
 
 
 @autoridades.before_request
@@ -55,18 +71,194 @@ def detail(autoridad_id):
     return render_template("autoridades/detail.jinja2", autoridad=autoridad)
 
 
+@autoridades.route("/autoridades/<int:autoridad_id>/audiencias_json", methods=["GET", "POST"])
+def audiencias_json(autoridad_id):
+    """Audiencias de una Autoridad en JSON"""
+    autoridad = Autoridad.query.get_or_404(autoridad_id)
+    audiencias = Audiencia.query.filter(Audiencia.autoridad == autoridad).filter_by(estatus="A")
+    # Listado
+    listado = []
+    desde = datetime.now()  # Desde este momento
+    for audiencia in audiencias.filter(Audiencia.tiempo >= desde).order_by(Audiencia.tiempo).limit(TARJETAS_LIMITE_REGISTROS).all():
+        listado.append(
+            {
+                "tiempo": audiencia.tiempo.strftime("%Y-%m-%d %H:%M"),
+                "tipo_audiencia": audiencia.tipo_audiencia,
+                "url": url_for("audiencias.detail", audiencia_id=audiencia.id),
+            }
+        )
+    # Breve
+    estilo = TARJETA_SATISFACTORIA
+    total = audiencias.count()
+    if total > 0:
+        breve = f"Total {total}. "
+        if len(listado) > 0:
+            breve += "Desde " + desde.strftime("%Y-%m-%d %H:%M") + ". "
+        else:
+            breve += "No hay eventos en la agenda de hoy."
+            estilo = TARJETA_ADVERTENCIA
+    else:
+        breve = "Nunca ha agendado Audiencias. "
+        estilo = TARJETA_VACIA
+    # Entregar JSON
+    return {
+        "titulo": "Agenda de Audiencias",
+        "breve": breve,
+        "listado": listado,
+        "url": url_for("audiencias.list_autoridad_audiencias", autoridad_id=autoridad_id),
+        "style": estilo,
+    }
+
+
+@autoridades.route("/autoridades/<int:autoridad_id>/edictos_json", methods=["GET", "POST"])
+def edictos_json(autoridad_id):
+    """Edictos de una Autoridad en JSON"""
+    autoridad = Autoridad.query.get_or_404(autoridad_id)
+    edictos = Edicto.query.filter(Edicto.autoridad == autoridad).filter_by(estatus="A")
+    # Listado
+    listado = []
+    for edicto in edictos.order_by(Edicto.creado.desc()).limit(TARJETAS_LIMITE_REGISTROS).all():
+        listado.append(
+            {
+                "expediente": edicto.expediente,
+                "descripcion": edicto.descripcion,
+                "url": url_for("edictos.detail", edicto_id=edicto.id),
+            }
+        )
+    # Breve
+    estilo = TARJETA_SATISFACTORIA
+    total = edictos.count()
+    if total > 0:
+        breve = f"Total {total}. "
+        cantidad = edictos.filter(Edicto.creado >= BREVE_DESDE_DATETIME).count()
+        if cantidad > 0:
+            breve += f"Se subieron {cantidad} en {BREVE_LIMITE_DIAS} días."
+        else:
+            breve += f"Ninguno en {BREVE_LIMITE_DIAS} días."
+            estilo = TARJETA_ADVERTENCIA
+    else:
+        breve = "Nunca a subido Edictos."
+        estilo = TARJETA_VACIA
+    # Entregar JSON
+    return {
+        "titulo": "Edictos",
+        "breve": breve,
+        "listado": listado,
+        "url": url_for("edictos.list_autoridad_edictos", autoridad_id=autoridad_id),
+        "style": estilo,
+    }
+
+
+@autoridades.route("/autoridades/<int:autoridad_id>/listas_de_acuerdos_json", methods=["GET", "POST"])
+def listas_de_acuerdos_json(autoridad_id):
+    """Listas de Acuerdos de una Autoridad en JSON"""
+    autoridad = Autoridad.query.get_or_404(autoridad_id)
+    listas_de_acuerdos = ListaDeAcuerdo.query.filter(ListaDeAcuerdo.autoridad == autoridad).filter_by(estatus="A")
+    # Listado
+    listado = []
+    for lista_de_acuerdo in listas_de_acuerdos.order_by(ListaDeAcuerdo.creado.desc()).limit(TARJETAS_LIMITE_REGISTROS).all():
+        listado.append(
+            {
+                "fecha": lista_de_acuerdo.fecha.strftime("%Y-%m-%d"),
+                "url": url_for("listas_de_acuerdos.detail", lista_de_acuerdo_id=lista_de_acuerdo.id),
+            }
+        )
+    # Breve
+    estilo = TARJETA_SATISFACTORIA
+    total = listas_de_acuerdos.count()
+    if total > 0:
+        breve = f"Total {total}. "
+    else:
+        breve = "Nunca a subido Listas de Acuerdos."
+        estilo = TARJETA_VACIA
+    # Entregar JSON
+    return {
+        "titulo": "Listas de Acuerdos",
+        "breve": breve,
+        "listado": listado,
+        "url": url_for("listas_de_acuerdos.list_autoridad_listas_de_acuerdos", autoridad_id=autoridad_id),
+        "style": estilo,
+    }
+
+
+@autoridades.route("/autoridades/<int:autoridad_id>/sentencias_json", methods=["GET", "POST"])
+def sentencias_json(autoridad_id):
+    """Sentencias de una Autoridad en JSON"""
+    autoridad = Autoridad.query.get_or_404(autoridad_id)
+    sentencias = Sentencia.query.filter(Sentencia.autoridad == autoridad).filter_by(estatus="A")
+    # Listado
+    listado = []
+    for sentencia in sentencias.order_by(Sentencia.creado.desc()).limit(TARJETAS_LIMITE_REGISTROS).all():
+        listado.append(
+            {
+                "sentencia": sentencia.sentencia,
+                "materia_tipo_juicio": sentencia.materia_tipo_juicio.descripcion,
+                "url": url_for("sentencias.detail", sentencia_id=sentencia.id),
+            }
+        )
+    # Breve
+    estilo = TARJETA_SATISFACTORIA
+    total = sentencias.count()
+    if total > 0:
+        breve = f"Total {total}. "
+        cantidad = sentencias.filter(Sentencia.creado >= BREVE_DESDE_DATETIME).count()
+        if cantidad > 0:
+            breve += f"Se subieron {cantidad} en {BREVE_LIMITE_DIAS} días."
+        else:
+            breve += f"Ninguno en {BREVE_LIMITE_DIAS} días."
+            estilo = TARJETA_ADVERTENCIA
+    else:
+        breve = "Nunca a subido V.P. de Sentencias."
+        estilo = TARJETA_VACIA
+    # Entregar JSON
+    return {
+        "titulo": "V.P. de Sentencias",
+        "breve": breve,
+        "listado": listado,
+        "url": url_for("sentencias.list_autoridad_sentencias", autoridad_id=autoridad_id),
+        "style": estilo,
+    }
+
+
 @autoridades.route("/autoridades/nuevo", methods=["GET", "POST"])
 @permission_required(MODULO, Permiso.CREAR)
 def new():
     """Nuevo Autoridad"""
-    form = AutoridadForm()
+    form = AutoridadNewForm()
     if form.validate_on_submit():
+        distrito = form.distrito.data
+        descripcion = form.descripcion.data.strip()
+        es_jurisdiccional = form.es_jurisdiccional.data
+        es_notaria = form.es_notaria.data
+        directorio = f"{distrito.nombre}/{descripcion}"
+        directorio_listas_de_acuerdos = ""
+        directorio_sentencias = ""
+        directorio_edictos = ""
+        directorio_glosas = ""
+        limite_dias_listas_de_acuerdos = 0
+        if es_jurisdiccional:
+            directorio_edictos = directorio
+            directorio_listas_de_acuerdos = directorio
+            directorio_sentencias = directorio
+            directorio_glosas = directorio
+            limite_dias_listas_de_acuerdos = 1
+        if es_notaria:
+            directorio_edictos = directorio
         autoridad = Autoridad(
-            distrito=form.distrito.data,
-            descripcion=form.descripcion.data,
-            descripcion_corta=form.descripcion_corta.data,
-            clave=form.clave.data,
-            es_jurisdiccional=form.es_jurisdiccional.data == 1,
+            distrito=distrito,
+            descripcion=descripcion,
+            descripcion_corta=form.descripcion_corta.data.strip(),
+            clave=form.clave.data.strip().upper(),
+            es_jurisdiccional=es_jurisdiccional,
+            es_notaria=es_notaria,
+            organo_jurisdiccional=form.organo_jurisdiccional.data,
+            materia=form.materia.data,
+            audiencia_categoria=form.audiencia_categoria.data,
+            directorio_listas_de_acuerdos=directorio_listas_de_acuerdos,
+            directorio_sentencias=directorio_sentencias,
+            directorio_edictos=directorio_edictos,
+            directorio_glosas=directorio_glosas,
+            limite_dias_listas_de_acuerdos=limite_dias_listas_de_acuerdos,
         )
         autoridad.save()
         bitacora = Bitacora(
@@ -86,13 +278,22 @@ def new():
 def edit(autoridad_id):
     """Editar Autoridad"""
     autoridad = Autoridad.query.get_or_404(autoridad_id)
-    form = AutoridadForm()
+    form = AutoridadEditForm()
     if form.validate_on_submit():
         autoridad.distrito = form.distrito.data
-        autoridad.descripcion = form.descripcion.data
-        autoridad.descripcion_corta = form.descripcion_corta.data
-        autoridad.clave = form.clave.data
+        autoridad.descripcion = form.descripcion.data.strip()
+        autoridad.descripcion_corta = form.descripcion_corta.data.strip()
+        autoridad.clave = form.clave.data.strip().upper()
         autoridad.es_jurisdiccional = form.es_jurisdiccional.data
+        autoridad.es_notaria = form.es_notaria.data
+        autoridad.organo_jurisdiccional = form.organo_jurisdiccional.data
+        autoridad.materia = form.materia.data
+        autoridad.audiencia_categoria = form.audiencia_categoria.data
+        autoridad.directorio_listas_de_acuerdos = form.directorio_listas_de_acuerdos.data.strip()
+        autoridad.directorio_sentencias = form.directorio_sentencias.data.strip()
+        autoridad.directorio_edictos = form.directorio_edictos.data.strip()
+        autoridad.directorio_glosas = form.directorio_glosas.data.strip()
+        autoridad.limite_dias_listas_de_acuerdos = form.limite_dias_listas_de_acuerdos.data
         autoridad.save()
         bitacora = Bitacora(
             modulo=Modulo.query.filter_by(nombre=MODULO).first(),
@@ -107,8 +308,16 @@ def edit(autoridad_id):
     form.descripcion.data = autoridad.descripcion
     form.descripcion_corta.data = autoridad.descripcion_corta
     form.clave.data = autoridad.clave
-    if autoridad.es_jurisdiccional:
-        form.es_jurisdiccional.data = 1
+    form.es_jurisdiccional.data = autoridad.es_jurisdiccional
+    form.es_notaria.data = autoridad.es_notaria
+    form.organo_jurisdiccional.data = autoridad.organo_jurisdiccional
+    form.materia.data = autoridad.materia
+    form.audiencia_categoria.data = autoridad.audiencia_categoria
+    form.directorio_listas_de_acuerdos.data = autoridad.directorio_listas_de_acuerdos
+    form.directorio_sentencias.data = autoridad.directorio_sentencias
+    form.directorio_edictos.data = autoridad.directorio_edictos
+    form.directorio_glosas.data = autoridad.directorio_glosas
+    form.limite_dias_listas_de_acuerdos.data = autoridad.limite_dias_listas_de_acuerdos
     return render_template("autoridades/edit.jinja2", form=form, autoridad=autoridad)
 
 
