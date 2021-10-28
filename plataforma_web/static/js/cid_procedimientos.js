40ce6882f0d8692b9f768b1eb55ad31e2e571a07
Lucia Aranda <lucyarandal@gmail.com>
2021-10-28 09:16:20 -0500

    Estos cambios no van aquí

--- plataforma_web/static/js/cid_procedimientos.js
+++ plataforma_web/static/js/cid_procedimientos.js
@@ -96,16 +96,6 @@ function llenar_control_cambios(contenido) {
     controlCambiosQuill.setContents(contenido);
 }
 
-function guardar_autorizaciones() {
-    autorizaciones = JSON.stringify(autorizacionesQuill.getContents());
-    $('#autorizaciones').val(autorizaciones);
-}
-
-// Llenar Control de Cambios
-function llenar_autorizaciones(contenido) {
-    autorizacionesQuill.setContents(contenido);
-}
-
 // Guardar Vista Previa
 function guardar_vista_previa() {
     console.log('OK Vista Previa');
