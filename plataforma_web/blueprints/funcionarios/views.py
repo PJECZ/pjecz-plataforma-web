78987a2ad520b237931be22f902dde3b15a8cf1a
Guillermo Valdés <guillermo.valdes@pjecz.gob.mx>
2021-10-29 16:31:13 -0500

    Avance funcionarios.

--- plataforma_web/blueprints/funcionarios/views.py
+++ plataforma_web/blueprints/funcionarios/views.py
@@ -2,11 +2,16 @@
 Funcionarios, vistas
 """
 from flask import Blueprint, flash, redirect, render_template, request, url_for
-from flask_login import login_required
+from flask_login import current_user, login_required
 
-from plataforma_web.blueprints.permisos.models import Permiso
+from lib.safe_string import safe_message
+
+from plataforma_web.blueprints.bitacoras.models import Bitacora
 from plataforma_web.blueprints.usuarios.decorators import permission_required
 from plataforma_web.blueprints.funcionarios.models import Funcionario
+from plataforma_web.blueprints.funcionarios.forms import FuncionarioForm
+from plataforma_web.blueprints.modulos.models import Modulo
+from plataforma_web.blueprints.permisos.models import Permiso
 
 MODULO = "FUNCIONARIOS"
 
@@ -50,3 +55,30 @@ def detail(funcionario_id):
     """Detalle de un Funcionario"""
     funcionario = Funcionario.query.get_or_404(funcionario_id)
     return render_template("funcionarios/detail.jinja2", funcionario=funcionario)
+
+
+@funcionarios.route("/funcionarios/nuevo", methods=["GET", "POST"])
+@permission_required(MODULO, Permiso.CREAR)
+def new():
+    """Nuevo Funcionario"""
+    form = FuncionarioForm()
+    if form.validate_on_submit():
+        funcionario = Funcionario(
+            nombres=form.nombres.data,
+            apellido_paterno=form.apellido_paterno.data,
+            apellido_materno=form.apellido_materno.data,
+            en_funciones=form.en_funciones.data,
+        )
+        funcionario.save()
+        bitacora = Bitacora(
+            modulo=Modulo.query.filter_by(nombre=MODULO).first(),
+            usuario=current_user,
+            descripcion=safe_message(f"Nuevo funcionario {funcionario.nombre}"),
+            url=url_for("funcionarios.detail", funcionario_id=funcionario.id),
+        )
+        bitacora.save()
+        flash(bitacora.descripcion, "success")
+        return redirect(bitacora.url)
+    return render_template("funcionarios/new.jinja2", form=form)
+
+
