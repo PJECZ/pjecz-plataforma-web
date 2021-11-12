98fc7b289a786e96d4ef6ac2ec4105de0f60aa0f
Guillermo Valdés <guillermo.valdes@pjecz.gob.mx>
2021-11-12 07:57:30 -0600

    Turnos listado con AJAX.

--- plataforma_web/blueprints/turnos/views.py
+++ plataforma_web/blueprints/turnos/views.py
@@ -6,9 +6,13 @@ from flask import Blueprint, flash, redirect, render_template, request, url_for
 from flask_login import login_required
 
 from lib import datatables
+
+from plataforma_web.blueprints.autoridades.models import Autoridad
 from plataforma_web.blueprints.permisos.models import Permiso
 from plataforma_web.blueprints.usuarios.decorators import permission_required
 from plataforma_web.blueprints.turnos.models import Turno
+from plataforma_web.blueprints.usuarios.models import Usuario
+from plataforma_web.blueprints.ventanillas.models import Ventanilla
 
 MODULO = "TURNOS"
 
@@ -31,6 +35,52 @@ def list_active():
         titulo="Turnos",
         estatus="A",
     )
+
+
+@turnos.route("/turnos/datatable_json", methods=["GET", "POST"])
+def datatable_json():
+    """DataTable JSON para listado de turnos"""
+    # Tomar parámetros de Datatables
+    draw, start, rows_per_page = datatables.get_parameters()
+    # Consultar
+    consulta = Turno.query
+    if "estatus" in request.form:
+        consulta = consulta.filter_by(estatus=request.form["estatus"])
+    else:
+        consulta = consulta.filter_by(estatus="A")
+    # if "autoridad_id" in request.form:
+    #    autoridad = Autoridad.query.get_or_404(request.form["autoridad_id"])
+    #    consulta = consulta.filter(Turno.autoridad == autoridad)
+    if "ventanilla_id" in request.form:
+        ventanilla = Ventanilla.query.get_or_404(request.form["ventanilla_id"])
+        consulta = consulta.filter(Turno.ventanilla == ventanilla)
+    if "estado" in request.form:
+        consulta = consulta.filter_by(estado=request.form["estado"])
+    if "tipo" in request.form:
+        consulta = consulta.filter_by(tipo=request.form["tipo"])
+    if "usuario_id" in request.form:
+        usuario = Usuario.query.get_or_404(request.form["usuario_id"])
+        consulta = consulta.filter(Turno.usuario == usuario)
+    registros = consulta.order_by(Turno.id.desc()).offset(start).limit(rows_per_page).all()
+    total = consulta.count()
+    # Elaborar datos para DataTable
+    data = []
+    for turno in registros:
+        data.append(
+            {
+                "autoridad_clave": turno.autoridad.clave,
+                "ventanilla_numero": turno.ventanilla.numero,
+                "detalle": {
+                    "numero": turno.numero,
+                    "url": url_for("turnos.detail", turno_id=turno.id),
+                },
+                "estado": turno.estado,
+                "tipo": turno.tipo,
+                "usuario_email": turno.usuario.email,
+            }
+        )
+    # Entregar JSON
+    return datatables.output(draw, total, data)
 
 
 @turnos.route("/turnos/<int:turno_id>")
