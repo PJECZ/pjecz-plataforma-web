57aa53d782a68ca141e89a0d1d493787f1c5de64
Guillermo Valdés <guillermo.valdes@pjecz.gob.mx>
2021-12-01 12:03:00 -0600

    Soportes Categorias.

---
+++ plataforma_web/blueprints/soportes_categorias/models.py
@@ -0,0 +1,25 @@
+"""
+Soportes Categorias, modelos
+"""
+from plataforma_web.extensions import db
+from lib.universal_mixin import UniversalMixin
+
+
+class SoporteCategoria(db.Model, UniversalMixin):
+    """SoporteCategoria"""
+
+    # Nombre de la tabla
+    __tablename__ = "soportes_categorias"
+
+    # Clave primaria
+    id = db.Column(db.Integer, primary_key=True)
+
+    # Columnas
+    nombre = db.Column(db.String(256), unique=True, nullable=False)
+
+    # Hijos
+    soportes_tickets = db.relationship("SoporteTicket", back_populates="soporte_categoria")
+
+    def __repr__(self):
+        """Representación"""
+        return "<SoporteCategoria>"
