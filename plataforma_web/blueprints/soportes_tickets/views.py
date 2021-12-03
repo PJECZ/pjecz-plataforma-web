72333fbc8b9350225c9039be6091922ab0b6cab9
Ricardo Valdes <ricardo.valdes@users.noreply.github.com>
2021-12-03 15:05:44 -0600

    Funcionalidad de Soporte Tickets completa

--- plataforma_web/blueprints/soportes_tickets/views.py
+++ plataforma_web/blueprints/soportes_tickets/views.py
@@ -1,7 +1,7 @@
 """
 Soportes Tickets, vistas
 """
-import json
+from datetime import datetime
 from flask import Blueprint, flash, redirect, render_template, request, url_for
 from flask_login import current_user, login_required
 
@@ -14,8 +14,9 @@ from plataforma_web.blueprints.permisos.models import Permiso
 from plataforma_web.blueprints.usuarios.decorators import permission_required
 from plataforma_web.blueprints.soportes_tickets.models import SoporteTicket
 from plataforma_web.blueprints.usuarios.models import Usuario
+from plataforma_web.blueprints.funcionarios.models import Funcionario
 
-from .forms import SoporteTicketEditForm, SoporteTicketNewForm
+from .forms import SoporteTicketEditForm, SoporteTicketNewForm, SoporteTicketCloseForm
 
 MODULO = "SOPORTES TICKETS"
 
@@ -32,19 +33,41 @@ def before_request():
 @soportes_tickets.route("/soportes_tickets")
 def list_active():
     """Listado de TODOS los Soportes Tickets activos"""
+    soportes_tickets_abiertos = []
+    soportes_tickets_procesando = []
+    soportes_tickets_terminados = []
+    soportes_tickets_cancelados = []
+    tipo_acceso = None
+
     # Query para administradores
     if current_user.can_admin(MODULO):
         soportes_tickets_abiertos = SoporteTicket.query.filter(SoporteTicket.estatus == "A").filter(SoporteTicket.estado=="ABIERTO").all()
         soportes_tickets_procesando = SoporteTicket.query.filter(SoporteTicket.estatus == "A").filter(SoporteTicket.estado=="TRABAJANDO").all()
         soportes_tickets_terminados = SoporteTicket.query.filter(SoporteTicket.estatus == "A").filter(SoporteTicket.estado=="CERRADO").order_by(SoporteTicket.creado.desc()).limit(50).all()
         soportes_tickets_cancelados = SoporteTicket.query.filter(SoporteTicket.estatus == "A").filter(SoporteTicket.estado=="CANCELADO").order_by(SoporteTicket.creado.desc()).limit(50).all()
+        tipo_acceso = "ADMIN"
 
     # Query para Técnicos
-    elif current_user.can_edit(MODULO) or current_user.can_insert(MODULO):
+    elif current_user.can_edit(MODULO):
         soportes_tickets_abiertos = SoporteTicket.query.filter(SoporteTicket.estatus == "A").filter(SoporteTicket.estado=="ABIERTO").all()
-        soportes_tickets_procesando = SoporteTicket.query.filter(SoporteTicket.estatus == "A").filter(SoporteTicket.estado=="TRABAJANDO").filter(SoporteTicket.funcionario_id == current_user.id).all()
-        soportes_tickets_terminados = SoporteTicket.query.filter(SoporteTicket.estatus == "A").filter(SoporteTicket.estado=="CERRADO").order_by(SoporteTicket.creado.desc()).limit(50).all()
-        soportes_tickets_cancelados = SoporteTicket.query.filter(SoporteTicket.estatus == "A").filter(SoporteTicket.estado=="CANCELADO").order_by(SoporteTicket.creado.desc()).limit(50).all()
+        funcionario = _current_user_in_funcionario()
+        if funcionario is False:
+            flash(f'No se encuentra el funcionario con el usuario ID: {current_user.id}.', 'warning')
+        soportes_tickets_procesando = SoporteTicket.query.filter(SoporteTicket.estatus == "A").filter(SoporteTicket.estado=="TRABAJANDO").filter(SoporteTicket.funcionario == funcionario).all()
+        soportes_tickets_terminados = SoporteTicket.query.filter(SoporteTicket.estatus == "A").filter(SoporteTicket.estado=="CERRADO").order_by(SoporteTicket.creado.desc()).filter(SoporteTicket.funcionario == funcionario).limit(50).all()
+        soportes_tickets_cancelados = SoporteTicket.query.filter(SoporteTicket.estatus == "A").filter(SoporteTicket.estado=="CANCELADO").order_by(SoporteTicket.creado.desc()).filter(SoporteTicket.funcionario == funcionario).limit(50).all()
+        tipo_acceso = "TECNICO"
+
+    # Query para Usuarios
+    elif current_user.can_view(MODULO):
+        soportes_tickets_abiertos = SoporteTicket.query.filter(SoporteTicket.estatus == "A").filter(SoporteTicket.estado=="ABIERTO").all()
+        soportes_tickets_procesando = SoporteTicket.query.filter(SoporteTicket.estatus == "A").filter(SoporteTicket.estado=="TRABAJANDO").filter(SoporteTicket.usuario_id == current_user.id).all()
+        soportes_tickets_terminados = SoporteTicket.query.filter(SoporteTicket.estatus == "A").filter(SoporteTicket.estado=="CERRADO").order_by(SoporteTicket.creado.desc()).filter(SoporteTicket.usuario_id == current_user.id).limit(50).all()
+        soportes_tickets_cancelados = SoporteTicket.query.filter(SoporteTicket.estatus == "A").filter(SoporteTicket.estado=="CANCELADO").order_by(SoporteTicket.creado.desc()).filter(SoporteTicket.usuario_id == current_user.id).limit(50).all()
+        tipo_acceso = "USUARIO"
+
+    else:
+        return redirect(url_for(''))
 
     # Query para Usuarios
     return render_template(
@@ -53,12 +76,10 @@ def list_active():
         soportes_tickets_procesando=soportes_tickets_procesando,
         soportes_tickets_terminados=soportes_tickets_terminados,
         soportes_tickets_cancelados=soportes_tickets_cancelados,
+        tipo_acceso=tipo_acceso,
         titulo="Soportes Tickets",
         estatus="A",
     )
-
-
-# List owned tickets
 
 
 @soportes_tickets.route("/soportes_tickets/<int:soporte_ticket_id>")
@@ -71,7 +92,7 @@ def detail(soporte_ticket_id):
 # Create a new ticket
 
 @soportes_tickets.route('/soportes_tickets/nuevo/<int:usuario_id>', methods=['GET', 'POST'])
-@permission_required(MODULO, Permiso.CREAR)
+@permission_required(MODULO, Permiso.VER)
 def new(usuario_id):
     """Nuevo Ticket creado por el usuario"""
     usuario = Usuario.query.get_or_404(usuario_id)
@@ -118,23 +139,55 @@ def edit(soporte_ticket_id):
 def take(soporte_ticket_id):
     """Tomar Ticket"""
     soporte_ticket = SoporteTicket.query.get_or_404(soporte_ticket_id)
-    # FIXME: el id del usuario esta en una tabla diferente al id del funcionario
-    soporte_ticket.funcionario_id = current_user.id
+    funcionario = _current_user_in_funcionario()
+    if funcionario is False:
+        flash(f'Ticket no asignado. No se encuentra el funcionario con el usuario ID: {current_user.id}.', 'warning')
+        return redirect(url_for('soportes_tickets.list_active'))
+    soporte_ticket.funcionario = funcionario
     soporte_ticket.estado = "TRABAJANDO"
     soporte_ticket.save()
-    flash(f'Ticket {soporte_ticket.id} asigando.', 'success')
+    flash(f'Ticket {soporte_ticket.id} tomado.', 'success')
     return redirect(url_for('soportes_tickets.list_active'))
 
 
-# Close a ticket
+@soportes_tickets.route('/soportes_tickets/cerrar/<int:soporte_ticket_id>', methods=['GET', 'POST'])
+@permission_required(MODULO, Permiso.MODIFICAR)
+def closes(soporte_ticket_id):
+    """Cerrar Ticket"""
+    soporte_ticket = SoporteTicket.query.get_or_404(soporte_ticket_id)
+    form = SoporteTicketCloseForm()
+    if form.validate_on_submit():
+        soporte_ticket.estado = "CERRADO"
+        soporte_ticket.soluciones = form.soluciones.data
+        soporte_ticket.resolucion = form.resolucion.data
+        soporte_ticket.save()
+        flash(f'Ticket {soporte_ticket.id} cerrado.', 'success')
+        return redirect(url_for('soportes_tickets.detail', soporte_ticket_id=soporte_ticket.id))
+    form.usuario.data = soporte_ticket.usuario.nombre
+    form.categoria.data = soporte_ticket.soporte_categoria
+    form.tecnico.data = soporte_ticket.funcionario
+    form.descripcion.data = soporte_ticket.descripcion
+    form.estado.data = soporte_ticket.estado
+    form.resolucion.data = datetime.now()
+    form.soluciones.data = soporte_ticket.soluciones
+    return render_template('soportes_tickets/close.jinja2', form=form, soporte_ticket=soporte_ticket)
 
-# Cancel a ticket
 
 @soportes_tickets.route('/soportes_tickets/cancelar/<int:soporte_ticket_id>', methods=['GET', 'POST'])
 @permission_required(MODULO, Permiso.MODIFICAR)
 def cancel(soporte_ticket_id):
-    pass
+    """Cancelar Ticket"""
+    soporte_ticket = SoporteTicket.query.get_or_404(soporte_ticket_id)
+    soporte_ticket.estado = "CANCELADO"
+    soporte_ticket.save()
+    flash(f'Ticket {soporte_ticket.id} cancelado.', 'success')
+    return redirect(url_for('soportes_tickets.list_active'))
 
 # Delete a ticket
 
 # Recover a ticket
+
+def _current_user_in_funcionario():
+    usuario = Usuario.query.get_or_404(current_user.id)
+    funcionario = Funcionario.query.filter(Funcionario.email == usuario.email).first()
+    return funcionario if funcionario else False
