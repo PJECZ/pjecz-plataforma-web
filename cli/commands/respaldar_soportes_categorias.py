a2f67a1ad0a21528b59df17e8f6b2d04670829bb
Guillermo Valdes <guillermo.valdes@pjecz.gob.mx>
2022-01-16 22:08:51 -0600

    Funciona respaldo de las categorias de soportes.

--- cli/commands/respaldar_soportes_categorias.py
+++ cli/commands/respaldar_soportes_categorias.py
@@ -9,4 +9,32 @@ from plataforma_web.blueprints.soportes_categorias.models import SoporteCategori
 
 
 def respaldar_soportes_categorias(salida: str = "soportes_categorias.csv"):
-    """Respaldar Soportes Categorias a un archivo CSV"""
+    """Respaldar categorias de soportes a un archivo CSV"""
+    ruta = Path(salida)
+    if ruta.exists():
+        click.echo(f"AVISO: {salida} existe, no voy a sobreescribirlo.")
+        return
+    click.echo("Respaldando tipos de peritos...")
+    contador = 0
+    soportes_categorias = SoporteCategoria.query.order_by(SoporteCategoria.id).all()
+    with open(ruta, "w", encoding="utf8") as puntero:
+        respaldo = csv.writer(puntero)
+        respaldo.writerow(
+            [
+                "soporte_categoria_id",
+                "nombre",
+                "estatus",
+            ]
+        )
+        for soporte_categoria in soportes_categorias:
+            respaldo.writerow(
+                [
+                    soporte_categoria.id,
+                    soporte_categoria.nombre,
+                    soporte_categoria.estatus,
+                ]
+            )
+            contador += 1
+            if contador % 100 == 0:
+                click.echo(f"  Van {contador}...")
+    click.echo(f"  {contador} en {ruta.name}")
