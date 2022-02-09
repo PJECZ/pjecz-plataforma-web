283d4f665782e99bb5ac941a21049d61296bbb78
Ricardo Valdes <ricardo.valdes@users.noreply.github.com>
2022-02-09 14:47:11 -0600

    Insertado del campo instrucciones en el módulo de soportes_categorias

--- plataforma_web/blueprints/soportes_categorias/forms.py
+++ plataforma_web/blueprints/soportes_categorias/forms.py
@@ -2,11 +2,12 @@
 Soportes Categorias, formularios
 """
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, TextAreaField
 from wtforms.validators import DataRequired, Length
 
 
 class SoporteCategoriaForm(FlaskForm):
     """ Formulario SoporteCategoria """
     nombre = StringField('Nombre', validators=[DataRequired(), Length(max=256)])
+    instrucciones = TextAreaField('Instrucciones')
     guardar = SubmitField('Guardar')
