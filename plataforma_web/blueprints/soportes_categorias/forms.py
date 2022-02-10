1c779e6e309a0fecf6a067e844a63686375c20c5
Ricardo Valdes <ricardo.valdes@users.noreply.github.com>
2022-02-10 14:15:50 -0600

    Corrección de un error en el merge

--- plataforma_web/blueprints/soportes_categorias/forms.py
+++ plataforma_web/blueprints/soportes_categorias/forms.py
@@ -3,7 +3,7 @@ Soportes Categorias, formularios
 """
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, TextAreaField
-from wtforms.validators import DataRequired, Length
+from wtforms.validators import DataRequired, Length, Optional
 
 
 
