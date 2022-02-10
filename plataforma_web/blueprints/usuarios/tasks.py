15638a143826bf1740858e4625e115794bcaab23
Guillermo Valdes <guillermo.valdes@pjecz.gob.mx>
2022-02-10 15:30:30 -0600

    Avance.

--- plataforma_web/blueprints/usuarios/tasks.py
+++ plataforma_web/blueprints/usuarios/tasks.py
@@ -1,7 +1,7 @@
 """
-Funcionarios, tareas para ejecutar en el fondo
+Usuarios, tareas para ejecutar en el fondo
 
-- enviar_reporte: Enviar via correo electronico el reporte de funcionarios
+- enviar_reporte: Enviar via correo electronico el reporte de usuarios
 - sincronizar: Sincronizar funcionarios con la API de RRHH Personal
 """
 import locale
@@ -17,14 +17,16 @@ from lib.tasks import set_task_progress, set_task_error
 from plataforma_web.app import create_app
 from plataforma_web.extensions import db
 
-from plataforma_web.blueprints.funcionarios.models import Funcionario
+from plataforma_web.blueprints.autoridades.models import Autoridad
+from plataforma_web.blueprints.oficinas.models import Oficina
+from plataforma_web.blueprints.usuarios.models import Usuario
 
 load_dotenv()  # Take environment variables from .env
 
 bitacora = logging.getLogger(__name__)
 bitacora.setLevel(logging.INFO)
 formato = logging.Formatter("%(asctime)s:%(levelname)s:%(message)s")
-empunadura = logging.FileHandler("funcionarios.log")
+empunadura = logging.FileHandler("usuarios.log")
 empunadura.setFormatter(formato)
 bitacora.addHandler(empunadura)
 
@@ -36,14 +38,14 @@ locale.setlocale(locale.LC_TIME, "es_MX.utf8")
 
 
 def enviar_reporte():
-    """Enviar reporte de funcionarios via correo electronico"""
+    """Enviar reporte de usuarios via correo electronico"""
 
     # Iniciar
     bitacora.info("Inicia enviar reporte")
 
     # Consultar
-    funcionarios = db.session.query(Funcionario).filter_by(estatus="A")
-    bitacora.info("Total: %s", funcionarios.count())
+    usuarios = db.session.query(Usuario).filter_by(estatus="A")
+    bitacora.info("Total: %s", usuarios.count())
 
     # Terminar
     set_task_progress(100)
@@ -53,10 +55,26 @@ def enviar_reporte():
 
 
 def sincronizar():
-    """Sincronizar funcionarios con la API de RRHH Personal"""
+    """Sincronizar usuarios con la API de RRHH Personal"""
 
     # Iniciar
     bitacora.info("Inicia sincronizar")
+
+    # Definir la autoridad NO DEFINIDO
+    autoridad_no_definido = Autoridad.query.filter_by(clave="ND").first()
+    if autoridad_no_definido is None:
+        mensaje = "No se encontró la autoridad NO DEFINIDO"
+        set_task_error(mensaje)
+        bitacora.error(mensaje)
+        return
+
+    # Definir la oficina NO DEFINIDO
+    oficina_no_definido = Oficina.query.filter_by(clave="ND").first()
+    if oficina_no_definido is None:
+        mensaje = "No se encontró la oficina NO DEFINIDO"
+        set_task_error(mensaje)
+        bitacora.error(mensaje)
+        return
 
     # Iniciar sesion
     base_url = os.getenv("RRHH_PERSONAL_API_URL")
@@ -79,8 +97,8 @@ def sincronizar():
     limit = 50
     offset = 0
     total = None
-    funcionarios_presentes_contador = 0
-    funcionarios_insertados_contador = 0
+    usuarios_presentes_contador = 0
+    usuarios_insertados_contador = 0
     personas_omitidas_contador = 0
     while True:
         # Llamar a la API v1 personas
@@ -103,19 +121,22 @@ def sincronizar():
         for persona_datos in data["items"]:
             curp = persona_datos["curp"]
             email = persona_datos["email"]
-            if curp != "" and email !="" and email.endswith("@coahuila.gob.mx"):
-                funcionario = Funcionario.query.filter(or_(Funcionario.curp == curp, Funcionario.email == email)).first()
-                if funcionario is None:
-                    funcionarios_insertados_contador += 1
-                    Funcionario(
+            if curp != "" and email != "" and email.endswith("@coahuila.gob.mx"):
+                usuario = Usuario.query.filter(or_(Usuario.curp == curp, Usuario.email == email)).first()
+                if usuario is None:
+                    Usuario(
+                        autoridad=autoridad_no_definido,
+                        oficina=oficina_no_definido,
                         nombres=persona_datos["nombres"],
                         apellido_paterno=persona_datos["apellido_primero"],
                         apellido_materno=persona_datos["apellido_segundo"],
                         curp=curp,
                         email=email,
+                        contrasena="",
                     ).save()
+                    usuarios_insertados_contador += 1
                 else:
-                    funcionarios_presentes_contador += 1
+                    usuarios_presentes_contador += 1
             else:
                 personas_omitidas_contador += 1
         # Saltar
@@ -123,8 +144,8 @@ def sincronizar():
             break
         offset += limit
     # Terminar
-    bitacora.info("Se han insertado %s funcionarios", funcionarios_insertados_contador)
-    bitacora.info("Hay %s personas ya presentes en funcionarios, se omiten", funcionarios_presentes_contador)
+    bitacora.info("Se han insertado %s funcionarios", usuarios_insertados_contador)
+    bitacora.info("Hay %s personas ya presentes en funcionarios, se omiten", usuarios_presentes_contador)
     bitacora.info("En %s personas no hay CURP o email coahuila.gob.mx", personas_omitidas_contador)
     set_task_progress(100)
     mensaje_final = "Terminado sincronizar satisfactoriamente"
