cb1e2a77042ba1af16fd3b1109a623a9156405fd
Guillermo Valdes <guillermo.valdes@pjecz.gob.mx>
2022-02-10 08:59:52 -0600

    Correcciones.

--- plataforma_web/blueprints/soportes_categorias/forms.py
+++ plataforma_web/blueprints/soportes_categorias/forms.py
@@ -3,11 +3,11 @@ Soportes Categorias, formularios
 """
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField, TextAreaField
-from wtforms.validators import DataRequired, Length
+from wtforms.validators import DataRequired, Length, Optional
 
 
 class SoporteCategoriaForm(FlaskForm):
     """ Formulario SoporteCategoria """
     nombre = StringField('Nombre', validators=[DataRequired(), Length(max=256)])
-    instrucciones = TextAreaField('Instrucciones')
+    instrucciones = TextAreaField('Instrucciones', validators=[Optional(), Length(max=2048)])
     guardar = SubmitField('Guardar')
