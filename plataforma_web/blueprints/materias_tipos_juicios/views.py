708659669ed0a3c519fa17c7fc9bba916a08c222
Guillermo Valdes <guillermo.valdes@pjecz.gob.mx>
2022-03-09 16:05:55 -0600

    Faltab 3 archivos de cambiar.

--- plataforma_web/blueprints/materias_tipos_juicios/views.py
+++ plataforma_web/blueprints/materias_tipos_juicios/views.py
@@ -5,7 +5,7 @@ import json
 from flask import Blueprint, flash, redirect, render_template, request, url_for
 from flask_login import current_user, login_required
 
-from lib.datatables import get_parameters, output
+from lib.datatables import get_datatable_parameters, output_datatable_json
 from lib.safe_string import safe_message, safe_string
 from plataforma_web.blueprints.usuarios.decorators import permission_required
 
