cb0dfcd256e4911962e10660796d859efe3169fd
Guillermo Valdes <guillermo.valdes@pjecz.gob.mx>
2022-03-31 10:41:10 -0600

    Sincronizar Centros de Trabajo listo.

--- tests/sincronizar_centros_trabajos.py
+++ tests/sincronizar_centros_trabajos.py
@@ -1,5 +1,5 @@
 """
-Sinconizar funcionarios con la API de RRHH Personal
+Sinconizar centros de trabajos con la API de RRHH Personal
 """
 import os
 import requests
@@ -7,9 +7,14 @@ from dotenv import load_dotenv
 from ratelimit import limits, sleep_and_retry
 from tabulate import tabulate
 
+from plataforma_web.app import create_app
+from plataforma_web.extensions import db
+from plataforma_web.blueprints.centros_trabajos.models import CentroTrabajo
+from plataforma_web.blueprints.distritos.models import Distrito
+
 load_dotenv()  # Take environment variables from .env
 
-LLAMADOS_CANTIDAD = 4
+LLAMADOS_CANTIDAD = 2
 CUATRO_SEGUNDOS = 4
 
 
@@ -44,10 +49,10 @@ def get_token(base_url, username, password):
 
 @sleep_and_retry
 @limits(calls=LLAMADOS_CANTIDAD, period=CUATRO_SEGUNDOS)
-def get_personas(base_url, token, limit, offset):
-    """Consultar personas"""
+def get_centros_trabajos(base_url, token, limit, offset):
+    """Consultar centros de trabajo"""
     response = requests.get(
-        url=f"{base_url}/v1/personas",
+        url=f"{base_url}/v1/centros_trabajos",
         headers={"authorization": f"Bearer {token}"},
         params={"limit": limit, "offset": offset},
     )
@@ -59,27 +64,18 @@ def get_personas(base_url, token, limit, offset):
     return respuesta["total"], respuesta["items"]
 
 
-@sleep_and_retry
-@limits(calls=LLAMADOS_CANTIDAD, period=CUATRO_SEGUNDOS)
-def get_historial_puestos(base_url, token, persona_id):
-    """Consultar historial de puestos, entrega un elemento (el mas reciente) de encontrarse"""
-    response = requests.get(
-        url=f"{base_url}/v1/historial_puestos",
-        headers={"authorization": f"Bearer {token}"},
-        params={"persona_id": persona_id, "limit": 1, "offset": 0},
-    )
-    if response.status_code != 200:
-        raise StatusCodeNot200Error(f"Error de status code: {response.status_code}")
-    respuesta = response.json()
-    if "total" not in respuesta or "items" not in respuesta:
-        raise ResponseJSONError("Error en la respuesta, falta el total o el items")
-    if respuesta["total"] > 0:
-        return respuesta["items"][0]  # Entrega el unico elemento del listado
-    return None
-
-
 def main():
     """Procedimiento principal"""
+    # Iniciar SQLAlchemy
+    app = create_app()
+    app.app_context().push()
+    db.app = app
+    # Consultar el distrito NO DEFINIDO
+    distrito_no_definido = Distrito.query.filter(Distrito.nombre == "NO DEFINIDO").first()
+    if distrito_no_definido is None:
+        print("Error: No se encontró el distrito NO DEFINIDO")
+        return
+    # Obtener variables de entorno
     base_url = os.getenv("RRHH_PERSONAL_API_URL", None)
     username = os.getenv("RRHH_PERSONAL_API_USERNAME", None)
     password = os.getenv("RRHH_PERSONAL_API_PASSWORD", None)
@@ -88,21 +84,37 @@ def main():
         limit = 10
         offset = 0
         total = None
+        insertados_contador = 0
+        actualizados_contador = 0
         while True:
-            total, personas = get_personas(base_url, token, limit, offset)
+            total, centros_trabajos = get_centros_trabajos(base_url, token, limit, offset)
             print(f"Voy en el offset {offset} de {total}...")
             datos = []
-            for persona in personas:
-                nombre = f"{persona['nombres']} {persona['apellido_primero']} {persona['apellido_segundo']}"
-                centro_trabajo_clave = None
-                historial_puesto = get_historial_puestos(base_url, token, persona["id"])
-                if historial_puesto:
-                    centro_trabajo_clave = historial_puesto["centro_trabajo"]
-                datos.append([nombre, centro_trabajo_clave])
-            print(tabulate(datos, headers=["Nombre", "Centro de Trabajo"]))
+            for centro_trabajo in centros_trabajos:
+                accion = ""
+                clave = centro_trabajo["clave"]
+                nombre = centro_trabajo["nombre"]
+                consulta = CentroTrabajo.query.filter(CentroTrabajo.clave == clave).first()
+                if consulta is None:
+                    accion = "Insertado"
+                    CentroTrabajo(
+                        clave=clave,
+                        nombre=nombre,
+                        telefono="ND",
+                        distrito=distrito_no_definido,
+                    ).save()
+                    insertados_contador += 1
+                elif nombre != consulta.nombre:
+                    accion = "Actualizado"
+                    consulta.nombre = nombre
+                    consulta.save()
+                    actualizados_contador += 1
+                datos.append([clave, nombre, accion])
+            print(tabulate(datos, headers=["Clave", "Nombre", "Acción"]))
             offset += limit
             if offset >= total:
                 break
+        print(f"Se insertaron {insertados_contador} y se actualizaron {actualizados_contador} centros de trabajo")
     except requests.ConnectionError as error:
         print(f"Error de conexion: {error}")
     except requests.Timeout as error:
