3e0dbe8372efbaf1ed3815be6a9299ecc0e51032
Guillermo Valdes <guillermo.valdes@pjecz.gob.mx>
2022-07-13 21:38:02 -0500

    Tarea en el fondo lista

---
+++ plataforma_web/blueprints/inv_custodias/tasks.py
@@ -0,0 +1,59 @@
+"""
+Inventarios Custodias, tareas en el fondo
+
+- actualizar: Actualizar las cantidades de equipos y fotos de las custodias
+"""
+import logging
+
+from lib.tasks import set_task_progress
+
+from plataforma_web.app import create_app
+from plataforma_web.extensions import db
+
+from plataforma_web.blueprints.inv_custodias.models import InvCustodia
+from plataforma_web.blueprints.inv_equipos.models import InvEquipo
+from plataforma_web.blueprints.inv_equipos_fotos.models import InvEquipoFoto
+
+bitacora = logging.getLogger(__name__)
+bitacora.setLevel(logging.INFO)
+formato = logging.Formatter("%(asctime)s:%(levelname)s:%(message)s")
+empunadura = logging.FileHandler("inv_custodias.log")
+empunadura.setFormatter(formato)
+bitacora.addHandler(empunadura)
+
+app = create_app()
+app.app_context().push()
+db.app = app
+
+
+def actualizar():
+    """Actualizar las cantidades de equipos y fotos de las custodias"""
+    contador = 0
+    # Recorrer todas las custodias activas
+    for inv_custodia in InvCustodia.query.filter_by(estatus="A").all():
+        equipos_cantidad = 0
+        equipos_fotos_cantidad = 0
+        # Recorrer los equipos de cada custodia
+        for inv_equipo in InvEquipo.query.filter_by(inv_custodia_id=inv_custodia.id).filter_by(estatus="A").all():
+            # Incrementar la cantidad de equipos
+            equipos_cantidad += 1
+            # Consultar la cantidad de fotos de los equipos
+            equipos_fotos_cantidad += InvEquipoFoto.query.filter_by(inv_equipo_id=inv_equipo.id).filter_by(estatus="A").count()
+        hay_que_actualizar = False
+        # Si es diferente, actualizar la cantidad de equipos de la custodia
+        if inv_custodia.equipos_cantidad != equipos_cantidad:
+            inv_custodia.equipos_cantidad = equipos_cantidad
+            hay_que_actualizar = True
+        # Si es diferente, actualizar la cantidad de fotos de la custodia
+        if inv_custodia.equipos_fotos_cantidad != equipos_fotos_cantidad:
+            inv_custodia.equipos_fotos_cantidad = equipos_fotos_cantidad
+            hay_que_actualizar = True
+        # Si hay que actualizar, guardar los cambios
+        if hay_que_actualizar:
+            inv_custodia.save()
+            contador += 1
+    # Terminar
+    set_task_progress(100)
+    mensaje_final = f"Se actualiazaron {contador} custodias"
+    bitacora.info(mensaje_final)
+    return mensaje_final
