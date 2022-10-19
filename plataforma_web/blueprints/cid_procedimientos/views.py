bcf19fd3e16742f1bf6c505bc0c832219c084479
Lucia Aranda <lucyarandal@gmail.com>
2022-10-19 10:03:31 -0500

    Filtros para procedimientos y formatos

--- plataforma_web/blueprints/cid_procedimientos/views.py
+++ plataforma_web/blueprints/cid_procedimientos/views.py
@@ -1,6 +1,7 @@
 """
 CID Procedimientos, vistas
 """
+from crypt import methods
 import json
 from delta import html
 from flask import abort, Blueprint, flash, redirect, render_template, request, url_for
