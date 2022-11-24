be46e59a98818cc217862e8dc010ef344ed15352
RicVal <none@none.com>
2022-11-24 12:51:25 -0600

    Respetan la ñ pero no los acentos

---
+++ cli/commands/cmd_autoridades.py
@@ -0,0 +1,46 @@
+"""
+Autoridades
+
+- normalizar: Actualiza el nombre de las autoridades con safe_string
+"""
+import click
+
+from lib.safe_string import safe_string
+
+from plataforma_web.app import create_app
+from plataforma_web.extensions import db
+
+from plataforma_web.blueprints.autoridades.models import Autoridad
+
+app = create_app()
+db.app = app
+
+
+@click.group()
+def cli():
+    """Autoridades"""
+
+
+@click.command()
+@click.option("--test", default=True, type=bool, help="Modo de prueba")
+def normalizar(test):
+    """Actualiza el nombre de las autoridades con safe_string"""
+    if test:
+        click.echo("-MODO PRUEBA-")
+
+    contador = 0
+    autoridades = Autoridad.query.order_by(Autoridad.id).all()
+    for autoridad in autoridades:
+        contador = contador + 1
+        nombre_autoridad_normalizado = safe_string(autoridad.descripcion_corta, do_unidecode=False) if "ñ" in autoridad.descripcion_corta else safe_string(autoridad.descripcion_corta)
+        if test:
+            click.echo(f"{autoridad.id:3} : {autoridad.descripcion_corta} --> {nombre_autoridad_normalizado}")
+        else:
+            autoridad.descripcion_corta = nombre_autoridad_normalizado
+            autoridad.save()
+
+    if not test:
+        click.echo(f"Se actualizaron {contador} registros. Se respeto la Ñ, pero se eliminaron acentos.")
+
+
+cli.add_command(normalizar)
