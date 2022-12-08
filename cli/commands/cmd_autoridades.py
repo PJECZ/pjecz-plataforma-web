d90c1e8b04079c3d5bc4d71b000c2026ffbd21fa
Guillermo Valdes <guillermo.valdes@pjecz.gob.mx>
2022-12-08 14:52:56 -0600

    Comandos click para normalizar distritos y autoridades

--- cli/commands/cmd_autoridades.py
+++ cli/commands/cmd_autoridades.py
@@ -22,22 +22,28 @@ def cli():
 
 
 @click.command()
-@click.option("--test", is_flag=True, help="Solo mostrar los cambios que se harian.")
-def normalizar(test):
+@click.option("--actualizar", is_flag=True, help="Actualizar la base de datos.", default=False)
+def normalizar(actualizar):
     """Normalizar la descripcion y la descripcion_corta con safe_string"""
+    if not actualizar:
+        click.echo("Modo de prueba. No se guardaran los cambios. Use --actualizar False para guardar.")
     contador = 0
     autoridades = Autoridad.query.order_by(Autoridad.id).filter_by(estatus="A").all()
     for autoridad in autoridades:
         descripcion_normalizado = safe_string(autoridad.descripcion, save_enie=True)
         descripcion_corta_normalizado = safe_string(autoridad.descripcion_corta, save_enie=True)
-        if autoridad.descripcion != descripcion_normalizado or autoridad.descripcion_corta != descripcion_corta_normalizado:
+        hay_cambios = False
+        if autoridad.descripcion != descripcion_normalizado:
             autoridad.descripcion = descripcion_normalizado
-            autoridad.descripcion_corta = descripcion_corta_normalizado
             click.echo(f"  {autoridad.descripcion} -> {descripcion_normalizado}")
+            hay_cambios = True
+        if autoridad.descripcion_corta != descripcion_corta_normalizado:
+            autoridad.descripcion_corta = descripcion_corta_normalizado
             click.echo(f"  {autoridad.descripcion_corta} -> {descripcion_corta_normalizado}")
-            if not test:
-                autoridad.save()
-                contador = contador + 1
+            hay_cambios = True
+        if hay_cambios and actualizar:
+            autoridad.save()
+            contador = contador + 1
     click.echo(f"Se actualizaron {contador} de {len(autoridades)} autoridades.")
 
 
--- cli/commands/cmd_distritos.py
+++ cli/commands/cmd_distritos.py
@@ -22,22 +22,28 @@ def cli():
 
 
 @click.command()
-@click.option("--test", is_flag=True, help="Solo mostrar los cambios que se harian.")
-def normalizar(test):
+@click.option("--actualizar", is_flag=True, help="Actualizar la base de datos.", default=False)
+def normalizar(actualizar):
     """Normalizar el nombre y el nombre_corto con safe_string"""
+    if not actualizar:
+        click.echo("Modo de prueba. No se guardaran los cambios. Use --actualizar False para guardar.")
     contador = 0
     distritos = Distrito.query.order_by(Distrito.id).filter_by(estatus="A").all()
     for distrito in distritos:
         nombre_normalizado = safe_string(distrito.nombre, save_enie=True)
         nombre_corto_normalizado = safe_string(distrito.nombre_corto, save_enie=True)
-        if distrito.nombre != nombre_normalizado or distrito.nombre_corto != nombre_corto_normalizado:
+        hay_cambios = False
+        if distrito.nombre != nombre_normalizado:
             distrito.nombre = nombre_normalizado
-            distrito.nombre_corto = nombre_corto_normalizado
             click.echo(f"  {distrito.nombre} -> {nombre_normalizado}")
+            hay_cambios = True
+        if distrito.nombre_corto != nombre_corto_normalizado:
+            distrito.nombre_corto = nombre_corto_normalizado
             click.echo(f"  {distrito.nombre_corto} -> {nombre_corto_normalizado}")
-            if not test:
-                distrito.save()
-                contador = contador + 1
+            hay_cambios = True
+        if hay_cambios and actualizar:
+            distrito.save()
+            contador = contador + 1
     click.echo(f"Se actualizaron {contador} de {len(distritos)} distritos.")
 
 
