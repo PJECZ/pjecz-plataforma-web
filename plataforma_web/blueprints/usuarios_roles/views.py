4e02974588a374f7e37eb4efce31dcc75c85e3c5
Guillermo Valdes <guillermo.valdes@pjecz.gob.mx>
2022-12-09 11:34:39 -0600

    Pasan las pruebas Roles ON/OFF

--- plataforma_web/blueprints/usuarios_roles/views.py
+++ plataforma_web/blueprints/usuarios_roles/views.py
@@ -42,7 +42,7 @@ def datatable_json():
         consulta = consulta.filter_by(usuario_id=request.form["usuario_id"])
     if "rol_id" in request.form:
         consulta = consulta.filter_by(rol_id=request.form["rol_id"])
-    registros = consulta.order_by(UsuarioRol.id.desc()).offset(start).limit(rows_per_page).all()
+    registros = consulta.order_by(UsuarioRol.descripcion).offset(start).limit(rows_per_page).all()
     total = consulta.count()
     # Elaborar datos para DataTable
     data = []
@@ -69,6 +69,77 @@ def datatable_json():
     return output_datatable_json(draw, total, data)
 
 
+@usuarios_roles.route("/usuarios_roles/datatable_toggle_estatus_json", methods=["GET", "POST"])
+def datatable_toggle_estatus_json():
+    """DataTable JSON que entrega los roles de un usuario donde se muestra el boton para activar o desactivar"""
+    # Tomar parámetros de Datatables
+    draw, start, rows_per_page = get_datatable_parameters()
+    # Consultar
+    consulta = UsuarioRol.query
+    if "rol_id" in request.form:
+        consulta = consulta.filter_by(rol_id=request.form["rol_id"])
+    if "usuario_id" in request.form:
+        consulta = consulta.filter_by(usuario_id=request.form["usuario_id"])
+    registros = consulta.order_by(UsuarioRol.descripcion).offset(start).limit(rows_per_page).all()
+    total = consulta.count()
+    # Elaborar datos para DataTable
+    data = []
+    for resultado in registros:
+        data.append(
+            {
+                "detalle": {
+                    "id": resultado.id,
+                    "url": url_for("usuarios_roles.detail", usuario_rol_id=resultado.id),
+                },
+                "usuario": {
+                    "email": resultado.usuario.email,
+                    "url": url_for("usuarios.detail", usuario_id=resultado.usuario_id) if current_user.can_view("USUARIOS") else "",
+                },
+                "usuario_nombre": resultado.usuario.nombre,
+                "usuario_puesto": resultado.usuario.puesto,
+                "rol": {
+                    "nombre": resultado.rol.nombre,
+                    "url": url_for("roles.detail", rol_id=resultado.rol_id) if current_user.can_view("ROLES") else "",
+                },
+                "toggle_estatus": {
+                    "id": resultado.id,
+                    "estatus": resultado.estatus,
+                    "url": url_for("usuarios_roles.toggle_estatus_json", usuario_rol_id=resultado.id),
+                },
+            }
+        )
+    # Entregar JSON
+    return output_datatable_json(draw, total, data)
+
+
+@usuarios_roles.route("/usuarios_roles/toggle_estatus_json/<int:usuario_rol_id>", methods=["GET", "POST"])
+@permission_required(MODULO, Permiso.MODIFICAR)
+def toggle_estatus_json(usuario_rol_id):
+    """Cambiar el estatus de un usuario-rol"""
+
+    # Consultar usuario-rol
+    usuario_rol = UsuarioRol.query.get_or_404(usuario_rol_id)
+    if usuario_rol is None:
+        return {"success": False, "message": "No encontrado"}
+
+    # Cambiar estatus a su opuesto
+    if usuario_rol.estatus == "A":
+        usuario_rol.estatus = "B"
+    else:
+        usuario_rol.estatus = "A"
+
+    # Guardar
+    usuario_rol.save()
+
+    # Entregar JSON
+    return {
+        "success": True,
+        "message": "Activo" if usuario_rol.estatus == "A" else "Inactivo",
+        "estatus": usuario_rol.estatus,
+        "id": usuario_rol.id,
+    }
+
+
 @usuarios_roles.route("/usuarios_roles")
 def list_active():
     """Listado de Usuarios Roles activos"""
@@ -92,24 +163,24 @@ def list_inactive():
     )
 
 
+@usuarios_roles.route("/usuarios_roles/usuario/<int:usuario_id>", methods=["GET", "POST"])
+@permission_required(MODULO, Permiso.CREAR)
+def list_usuario(usuario_id):
+    """Listado de los roles de un usuario donde se muestra el boton para activar o desactivar"""
+    usuario = Usuario.query.get_or_404(usuario_id)
+    return render_template(
+        "usuarios_roles/list_usuario.jinja2",
+        filtros=json.dumps({"usuario_id": usuario_id}),
+        titulo=f"Roles de {usuario.email}",
+        usuario=usuario,
+    )
+
+
 @usuarios_roles.route("/usuarios_roles/<int:usuario_rol_id>")
 def detail(usuario_rol_id):
     """Detalle de un Usuario Rol"""
     usuario_rol = UsuarioRol.query.get_or_404(usuario_rol_id)
     return render_template("usuarios_roles/detail.jinja2", usuario_rol=usuario_rol)
-
-
-@usuarios_roles.route("/usuarios_roles/usuario/<int:usuario_id>", methods=["GET", "POST"])
-@permission_required(MODULO, Permiso.CREAR)
-def list_usuario(usuario_id):
-    """Listado de Usuarios-Roles de un Usuario"""
-    usuario = Usuario.query.get_or_404(usuario_id)
-    return render_template(
-        "usuarios_roles/list_usuario.jinja2",
-        filtros=json.dumps({"usuario_id": usuario_id}),
-        titulo="Usuarios Roles de un Usuario",
-        usuario=usuario,
-    )
 
 
 @usuarios_roles.route("/usuarios_roles/nuevo_con_usuario/<int:usuario_id>", methods=["GET", "POST"])
