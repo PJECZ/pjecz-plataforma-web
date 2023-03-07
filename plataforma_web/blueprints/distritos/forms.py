f44bbbb2849db5558f6fd84a91de06c5edaf107d
Guillermo Valdes <guillermo.valdes@pjecz.gob.mx>
2023-03-07 15:23:46 -0600

    Booleanos en distritos

--- plataforma_web/blueprints/distritos/forms.py
+++ plataforma_web/blueprints/distritos/forms.py
@@ -12,5 +12,7 @@ class DistritoForm(FlaskForm):
     clave = StringField("Clave (única, máximo 16 caracteres)", validators=[DataRequired(), Length(max=16)])
     nombre = StringField("Nombre", validators=[DataRequired(), Length(max=256)])
     nombre_corto = StringField("Nombre corto", validators=[Optional(), Length(max=64)])
-    es_distrito_judicial = BooleanField("Es judicial", validators=[Optional()])
+    es_distrito_judicial = BooleanField("Es Distrito Judicial", validators=[Optional()])
+    es_distrito = BooleanField("NUEVO Es Distrito (geográfico)", validators=[Optional()])
+    es_jurisdiccional = BooleanField("NUEVO Es Jurisdiccional", validators=[Optional()])
     guardar = SubmitField("Guardar")
