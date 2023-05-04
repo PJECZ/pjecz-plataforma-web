e6a4ef33514ebe4171cededebc7c82006183a94f
Guillermo Valdes <guillermo.valdes@pjecz.gob.mx>
2023-05-04 14:25:29 -0600

    Se corrige safe_expediente y se crea su test

--- tests/safe_expediente.py
+++ tests/safe_expediente.py
@@ -1,32 +1,28 @@
 """
 Testing lib/safe_string.py safe_expediente function
 """
-import re
 import unittest
 
-from lib.safe_string import EXPEDIENTE_REGEXP, safe_expediente
+from lib.safe_string import safe_expediente
 
 
 class TestSafeExpediente(unittest.TestCase):
     """Test Safe Expediente"""
 
     strings = [
+        ("1/2020", "1/2020"),
+        ("12/2020", "12/2020"),
         ("123/2020", "123/2020"),
-        ("   123/2020", "123/2020"),
-        ("1/2020  ", "1/2020"),
         ("1/2020-II", "1/2020-II"),
-        ("1/2020-III", "1/2020-III"),
+        ("1/2020-II-2", "1/2020-II-2"),
+        ("1/2020-F2", "1/2020-F2"),
     ]
 
     def test_safe_expediente(self):
         """Test Safe Expediente"""
 
         for string, expected in self.strings:
-            try:
-                clean_reg_exp = safe_expediente(string)
-            except (IndexError, ValueError):
-                self.assertEqual(True, False)
-            self.assertEqual(re.match(EXPEDIENTE_REGEXP, clean_reg_exp) is not None, True)
+            self.assertEqual(safe_expediente(string), expected)
 
 
 if __name__ == "__main__":
