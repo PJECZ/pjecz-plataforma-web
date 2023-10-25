56bfabdaac958bf09ff0cb2844a24cc01a70c671
RicVal <none@none.com>
2023-10-25 11:01:42 -0600

    Listado de estadística sin datos reales

--- plataforma_web/blueprints/arc_estadisticas/views.py
+++ plataforma_web/blueprints/arc_estadisticas/views.py
@@ -6,12 +6,16 @@ from datetime import date, timedelta
 from flask import Blueprint, render_template, request, url_for, flash, redirect
 from flask_login import current_user, login_required
 
+from lib.datatables import get_datatable_parameters, output_datatable_json
+
 from plataforma_web.blueprints.permisos.models import Permiso
 from plataforma_web.blueprints.usuarios.decorators import permission_required
 
 from plataforma_web.blueprints.arc_estadisticas.forms import ArcEstadisticasForm
 from plataforma_web.blueprints.arc_solicitudes.models import ArcSolicitud
 from plataforma_web.blueprints.arc_remesas.models import ArcRemesa
+from plataforma_web.blueprints.distritos.models import Distrito
+from plataforma_web.blueprints.autoridades.models import Autoridad
 
 MODULO = "ARC ESTADISTICAS"
 
@@ -58,7 +62,14 @@ def report_solicitudes_remesas():
         "arc_estadisticas/report_solicitudes_remesas.jinja2",
         solicitudes=solicitudes,
         remesas=remesas,
-        filtros_por_distrito=json.dumps(
+        filtros_por_distritos=json.dumps(
+            {
+                "estatus": "A",
+                "fecha_desde": fecha_desde.strftime("%Y-%m-%d"),
+                "fecha_hasta": fecha_hasta.strftime("%Y-%m-%d"),
+            }
+        ),
+        filtros_por_juzgados=json.dumps(
             {
                 "estatus": "A",
                 "fecha_desde": fecha_desde.strftime("%Y-%m-%d"),
@@ -67,3 +78,69 @@ def report_solicitudes_remesas():
         ),
         form=form,
     )
+
+
+@arc_estadisticas.route("/arc_estadisticas/datatable_json_solicitudes_remesas_por_distrito", methods=["GET", "POST"])
+def datatable_json_solicitudes_remesas_distrito():
+    """DataTable JSON para listado de solicitudes y remesas por distrito"""
+    # Tomar parámetros de Datatables
+    draw, start, rows_per_page = get_datatable_parameters()
+    # Consultar
+    consulta = Distrito.query
+    consulta = consulta.filter_by(es_distrito=True).filter_by(es_distrito_judicial=True).filter_by(es_jurisdiccional=True)
+    if "estatus" in request.form:
+        consulta = consulta.filter_by(estatus=request.form["estatus"])
+    else:
+        consulta = consulta.filter_by(estatus="A")
+    # if "fecha_desde" in request.form:
+    #     consulta = consulta.filter(ListaDeAcuerdo.fecha >= request.form["fecha_desde"])
+    # if "fecha_hasta" in request.form:
+    #     consulta = consulta.filter(ListaDeAcuerdo.fecha <= request.form["fecha_hasta"])
+    registros = consulta.order_by(Distrito.nombre_corto).offset(start).limit(rows_per_page).all()
+    total = consulta.count()
+    # Elaborar datos para DataTable
+    data = []
+    for distrito in registros:
+        data.append(
+            {
+                "clave": distrito.clave,
+                "nombre_corto": distrito.nombre_corto,
+                "solicitudes": 25,
+                "remesas": 10,
+            }
+        )
+    # Entregar JSON
+    return output_datatable_json(draw, total, data)
+
+
+@arc_estadisticas.route("/arc_estadisticas/datatable_json_solicitudes_remesas_por_juzgado", methods=["GET", "POST"])
+def datatable_json_solicitudes_remesas_juzgado():
+    """DataTable JSON para listado de solicitudes y remesas por distrito"""
+    # Tomar parámetros de Datatables
+    draw, start, rows_per_page = get_datatable_parameters()
+    # Consultar
+    consulta = Autoridad.query
+    consulta = consulta.filter_by(es_archivo_solicitante=True)
+    if "estatus" in request.form:
+        consulta = consulta.filter_by(estatus=request.form["estatus"])
+    else:
+        consulta = consulta.filter_by(estatus="A")
+    # if "fecha_desde" in request.form:
+    #     consulta = consulta.filter(ListaDeAcuerdo.fecha >= request.form["fecha_desde"])
+    # if "fecha_hasta" in request.form:
+    #     consulta = consulta.filter(ListaDeAcuerdo.fecha <= request.form["fecha_hasta"])
+    registros = consulta.order_by(Autoridad.clave).offset(start).limit(rows_per_page).all()
+    total = consulta.count()
+    # Elaborar datos para DataTable
+    data = []
+    for juzgado in registros:
+        data.append(
+            {
+                "clave": juzgado.clave,
+                "nombre_corto": juzgado.descripcion_corta,
+                "solicitudes": 5,
+                "remesas": 3,
+            }
+        )
+    # Entregar JSON
+    return output_datatable_json(draw, total, data)
