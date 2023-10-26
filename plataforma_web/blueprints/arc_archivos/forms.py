c780ed2df50545d4ef2f57b326f9101d7269002d
RicVal <none@none.com>
2023-10-26 12:30:40 -0600

    stats por distritos en solicitudes y remesas

---
+++ plataforma_web/blueprints/arc_archivos/forms.py
@@ -0,0 +1,16 @@
+"""
+Arc Archivo, formularios
+"""
+from flask_wtf import FlaskForm
+from wtforms import SubmitField, DateField
+from wtforms.validators import DataRequired
+
+
+class ArcEstadisticasDateRangeForm(FlaskForm):
+    """Formulario para estadísticas por rangos de fechas"""
+
+    fecha_desde = DateField("Desde", validators=[DataRequired()])
+    fecha_hasta = DateField("Hasta", validators=[DataRequired()])
+    totales = SubmitField("Totales")
+    por_distritos = SubmitField("Por Distritos")
+    por_instancias = SubmitField("Por Instancias")
