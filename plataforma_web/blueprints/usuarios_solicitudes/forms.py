2bcd6b71a72dffa5afb02c00407bf12903b295c7
Guillermo Valdes <guillermo@movimientolibre.com>
2024-01-16 10:56:24 -0600

    Listas las mejoras

--- plataforma_web/blueprints/usuarios_solicitudes/forms.py
+++ plataforma_web/blueprints/usuarios_solicitudes/forms.py
@@ -3,34 +3,37 @@ Usuarios-solicitudes, formularios
 """
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
-from wtforms.validators import Length, DataRequired
+from wtforms.validators import Length, DataRequired, Regexp, Email
+
+TELEFONO_CELULAR_REGEX = r"^\d{10}$"
+TOKEN_REGEX = r"^\d{6}$"
 
 
 class UsuarioSolicitudNewForm(FlaskForm):
     """Formulario UsuarioRol"""
 
-    usuario_nombre = StringField("Usuario Nombre")  # Read only
-    usuario_email = StringField("Usuario email")  # Read only
-    email_personal = StringField("E-Mail Personal", validators=[Length(max=64)])
-    telefono_celular = StringField("Teléfono Celular", validators=[Length(max=16)])
+    usuario_nombre = StringField("Tu nombre completo")  # Read only
+    usuario_email = StringField("Tu correo electrónico institucional")  # Read only
+    email_personal = StringField("Correo electrónico personal", validators=[DataRequired(), Email()])
+    telefono_celular = StringField("Teléfono celular", validators=[DataRequired(), Regexp(TELEFONO_CELULAR_REGEX)])
     enviar = SubmitField("Enviar")
 
 
 class UsuarioSolicitudValidateTokenEmailForm(FlaskForm):
     """Formulario UsuarioRol"""
 
-    usuario_nombre = StringField("Usuario Nombre")  # Read only
-    usuario_email = StringField("Usuario email")  # Read only
-    email_personal = StringField("E-Mail Personal")  # Read only
-    token_email = StringField("Token - E-Mail Personal", validators=[DataRequired(), Length(max=6)])
+    usuario_nombre = StringField("Tu nombre completo")  # Read only
+    usuario_email = StringField("Tu correo electrónico institucional")  # Read only
+    email_personal = StringField("Correo electrónico personal")  # Read only
+    token_email = StringField("Token recibido", validators=[DataRequired(), Regexp(TOKEN_REGEX)])
     validar = SubmitField("Validar")
 
 
 class UsuarioSolicitudValidateTokenTelefonoCelularForm(FlaskForm):
     """Formulario UsuarioRol"""
 
-    usuario_nombre = StringField("Usuario Nombre")  # Read only
-    usuario_email = StringField("Usuario email")  # Read only
+    usuario_nombre = StringField("Tu nombre completo")  # Read only
+    usuario_email = StringField("Tu correo electrónico institucional")  # Read only
     telefono_celular = StringField("Teléfono Celular")  # Read only
-    token_telefono_celular = StringField("Token - Teléfono Celular", validators=[DataRequired(), Length(max=6)])
+    token_telefono_celular = StringField("Token recibido", validators=[DataRequired(), Regexp(TOKEN_REGEX)])
     validar = SubmitField("Validar")
