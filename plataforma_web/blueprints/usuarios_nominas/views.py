bc7842b62a0d89979529f172e66d2d6600c017cb
RicVal <none@none.com>
2024-01-19 15:08:44 -0600

    quitando cosas obsoletas

--- plataforma_web/blueprints/usuarios_nominas/views.py
+++ plataforma_web/blueprints/usuarios_nominas/views.py
@@ -2,15 +2,11 @@
 Usuarios Nóminas, vistas
 """
 import json
-from datetime import datetime
-from flask import Blueprint, flash, redirect, render_template, request, url_for
+from flask import Blueprint, render_template, request
 from flask_login import current_user, login_required
 
 from lib.datatables import get_datatable_parameters, output_datatable_json
-from lib.safe_string import safe_string, safe_message
 
-from plataforma_web.blueprints.bitacoras.models import Bitacora
-from plataforma_web.blueprints.modulos.models import Modulo
 from plataforma_web.blueprints.permisos.models import Permiso
 from plataforma_web.blueprints.usuarios.decorators import permission_required
 from plataforma_web.blueprints.usuarios_nominas.models import UsuarioNomina
