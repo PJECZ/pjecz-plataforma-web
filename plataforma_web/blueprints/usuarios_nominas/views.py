3c9d77fa186e1bd346cd5c372422e0f0ee792e9a
RicVal <none@none.com>
2024-01-25 14:55:53 -0600

    Nuevas ligas de descarga de timbres

--- plataforma_web/blueprints/usuarios_nominas/views.py
+++ plataforma_web/blueprints/usuarios_nominas/views.py
@@ -2,10 +2,14 @@
 Usuarios Nóminas, vistas
 """
 import json
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, flash, make_response, redirect, url_for, current_app
 from flask_login import current_user, login_required
 
 from lib.datatables import get_datatable_parameters, output_datatable_json
+from lib.exceptions import MyBucketNotFoundError, MyFileNotFoundError, MyNotValidParamError
+from lib.google_cloud_storage import get_blob_name_from_url, get_file_from_gcs
+
+# from lib.safe_string import safe_curp
 
 from plataforma_web.blueprints.permisos.models import Permiso
 from plataforma_web.blueprints.usuarios.decorators import permission_required
@@ -36,7 +40,7 @@ def datatable_json():
         consulta = consulta.filter_by(estatus="A")
     if "usuario_id" in request.form:
         consulta = consulta.filter(UsuarioNomina.usuario_id == current_user.id)
-    registros = consulta.order_by(UsuarioNomina.id.desc()).offset(start).limit(rows_per_page).all()
+    registros = consulta.order_by(UsuarioNomina.fecha.desc()).offset(start).limit(rows_per_page).all()
     total = consulta.count()
     # Elaborar datos para DataTable
     data = []
@@ -49,11 +53,11 @@ def datatable_json():
                 },
                 "pdf": {
                     "archivo_pdf": resultado.archivo_pdf,
-                    "url_pdf": resultado.url_pdf,
+                    "url_pdf": url_for("usuarios_nominas.download_pdf", usuario_nomina_id=resultado.id),
                 },
                 "xml": {
                     "archivo_xml": resultado.archivo_xml,
-                    "url_xml": resultado.url_xml,
+                    "url_xml": url_for("usuarios_nominas.download_xml", usuario_nomina_id=resultado.id),
                 },
             }
         )
@@ -70,3 +74,71 @@ def list_active():
         titulo=f"Recibos de Nómina para {current_user.nombre}",
         estatus="A",
     )
+
+
+@usuarios_nominas.route("/usuarios_nominas/<int:usuario_nomina_id>/pdf")
+def download_pdf(usuario_nomina_id):
+    """Descargar el archivo PDF de un Timbrado"""
+
+    # Consultar el Timbrado
+    usuario_nomina = UsuarioNomina.query.get_or_404(usuario_nomina_id)
+
+    # Si no tiene URL, redirigir a la página de detalle
+    if usuario_nomina.url_pdf == "":
+        flash("El usuario_nomina no tiene un archivo PDF", "warning")
+        return redirect(url_for("usuarios_nominas.detail", nomina_id=usuario_nomina.id))
+
+    # Si no tiene nombre para el archivo en archivo_pdf, elaborar uno con el UUID
+    descarga_nombre = usuario_nomina.archivo_pdf
+    if descarga_nombre == "":
+        descarga_nombre = f"{usuario_nomina.tfd_uuid}.pdf"
+
+    # Obtener el contenido del archivo desde Google Storage
+    try:
+        descarga_contenido = get_file_from_gcs(
+            bucket_name=current_app.config["CLOUD_STORAGE_DEPOSITO"],
+            blob_name=get_blob_name_from_url(usuario_nomina.url_pdf),
+        )
+    except (MyBucketNotFoundError, MyFileNotFoundError, MyNotValidParamError) as error:
+        flash(str(error), "danger")
+        return redirect(url_for("usuarios_nominas.detail", usuario_nomina_id=usuario_nomina.id))
+
+    # Descargar un archivo PDF
+    response = make_response(descarga_contenido)
+    response.headers["Content-Type"] = "application/pdf"
+    response.headers["Content-Disposition"] = f"attachment; filename={descarga_nombre}"
+    return response
+
+
+@usuarios_nominas.route("/usuarios_nominas/<int:usuario_nomina_id>/xml")
+def download_xml(usuario_nomina_id):
+    """Descargar el archivo XML de un Timbrado"""
+
+    # Consultar el Timbrado
+    usuario_nomina = UsuarioNomina.query.get_or_404(usuario_nomina_id)
+
+    # Si no tiene URL, redirigir a la página de detalle
+    if usuario_nomina.url_xml == "":
+        flash("El usuario_nomina no tiene un archivo XML", "warning")
+        return redirect(url_for("usuarios_nominas.detail", nomina_id=usuario_nomina.id))
+
+    # Si no tiene nombre para el archivo en archivo_xml, elaborar uno con el UUID
+    descarga_nombre = usuario_nomina.archivo_xml
+    if descarga_nombre == "":
+        descarga_nombre = f"{usuario_nomina.tfd_uuid}.xml"
+
+    # Obtener el contenido del archivo desde Google Storage
+    try:
+        descarga_contenido = get_file_from_gcs(
+            bucket_name=current_app.config["CLOUD_STORAGE_DEPOSITO"],
+            blob_name=get_blob_name_from_url(usuario_nomina.url_xml),
+        )
+    except (MyBucketNotFoundError, MyFileNotFoundError, MyNotValidParamError) as error:
+        flash(str(error), "danger")
+        return redirect(url_for("usuarios_nominas.detail", usuario_nomina_id=usuario_nomina.id))
+
+    # Descargar un archivo XML
+    response = make_response(descarga_contenido)
+    response.headers["Content-Type"] = "text/xml"
+    response.headers["Content-Disposition"] = f"attachment; filename={descarga_nombre}"
+    return response
