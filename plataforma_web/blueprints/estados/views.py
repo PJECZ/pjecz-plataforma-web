792c4e0d5aecdcc941adbb0c5d19c90fc118320d
RicVal <none@none.com>
2024-04-25 11:23:10 -0600

    List y Detail para estados

--- plataforma_web/blueprints/estados/views.py
+++ plataforma_web/blueprints/estados/views.py
@@ -7,7 +7,7 @@ from flask import Blueprint, flash, redirect, render_template, request, url_for
 from flask_login import current_user, login_required
 
 from lib.datatables import get_datatable_parameters, output_datatable_json
-from lib.safe_string import safe_string, safe_message
+from lib.safe_string import safe_string
 
 from plataforma_web.blueprints.bitacoras.models import Bitacora
 from plataforma_web.blueprints.modulos.models import Modulo
@@ -38,6 +38,10 @@ def datatable_json():
         consulta = consulta.filter_by(estatus=request.form["estatus"])
     else:
         consulta = consulta.filter_by(estatus="A")
+    if "clave" in request.form:
+        consulta = consulta.filter_by(clave=safe_string(request.form["clave"]))
+    if "nombre" in request.form:
+        consulta = consulta.filter(Estado.nombre.contains(safe_string(request.form["nombre"])))
     registros = consulta.order_by(Estado.id).offset(start).limit(rows_per_page).all()
     total = consulta.count()
     # Elaborar datos para DataTable
