b90f04684408cc310e0e7ea35c8a7eccf3a128d3
RicVal <none@none.com>
2024-04-29 16:11:30 -0600

    Vista de Exhortos y Partes

--- plataforma_web/blueprints/exh_exhortos/views.py
+++ plataforma_web/blueprints/exh_exhortos/views.py
@@ -25,3 +25,53 @@ exh_exhortos = Blueprint("exh_exhortos", __name__, template_folder="templates")
 @permission_required(MODULO, Permiso.VER)
 def before_request():
     """Permiso por defecto"""
+
+
+@exh_exhortos.route('/exh_exhortos/datatable_json', methods=['GET', 'POST'])
+def datatable_json():
+    """DataTable JSON para listado de Exh Exhortos"""
+    # Tomar parámetros de Datatables
+    draw, start, rows_per_page = get_datatable_parameters()
+    # Consultar
+    consulta = ExhExhorto.query
+    if 'estatus' in request.form:
+        consulta = consulta.filter_by(estatus=request.form['estatus'])
+    else:
+        consulta = consulta.filter_by(estatus='A')
+    registros = consulta.order_by(ExhExhorto.id).offset(start).limit(rows_per_page).all()
+    total = consulta.count()
+    # Elaborar datos para DataTable
+    data = []
+    for resultado in registros:
+        data.append(
+            {
+                'detalle': {
+                    'nombre': resultado.id,
+                    'url': url_for('exh_exhortos.detail', exh_exhorto_id=resultado.id),
+                },
+                'UUID': resultado.exhorto_origen_id,
+                'juzgado_origen': resultado.juzgado_origen_id,
+                'juzgado_nombre': resultado.juzgado_origen_nombre,
+            }
+        )
+    # Entregar JSON
+    return output_datatable_json(draw, total, data)
+
+
+@exh_exhortos.route('/exh_exhortos')
+def list_active():
+    """Listado de Exh Exhortos activos"""
+    return render_template(
+        'exh_exhortos/list.jinja2',
+        filtros=json.dumps({'estatus': 'A'}),
+        titulo='Exhortos',
+        estatus='A',
+    )
+
+
+@exh_exhortos.route('/exh_exhortos/<int:exh_exhorto_id>')
+def detail(exh_exhorto_id):
+    """ Detalle de un Exh Exhorto """
+    exh_exhorto = ExhExhorto.query.get_or_404(exh_exhorto_id)
+    return render_template('exh_exhortos/detail.jinja2', exh_exhorto=exh_exhorto)
+
--- plataforma_web/blueprints/exh_exhortos_partes/views.py
+++ plataforma_web/blueprints/exh_exhortos_partes/views.py
@@ -25,3 +25,50 @@ exh_exhortos_partes = Blueprint("exh_exhortos_partes", __name__, template_folder
 @permission_required(MODULO, Permiso.VER)
 def before_request():
     """Permiso por defecto"""
+
+@exh_exhortos_partes.route('/exh_exhortos_partes/datatable_json', methods=['GET', 'POST'])
+def datatable_json():
+    """DataTable JSON para listado de Exh Exhortos Partes"""
+    # Tomar parámetros de Datatables
+    draw, start, rows_per_page = get_datatable_parameters()
+    # Consultar
+    consulta = ExhExhortoParte.query
+    if 'estatus' in request.form:
+        consulta = consulta.filter_by(estatus=request.form['estatus'])
+    else:
+        consulta = consulta.filter_by(estatus='A')
+    registros = consulta.order_by(ExhExhortoParte.id).offset(start).limit(rows_per_page).all()
+    total = consulta.count()
+    # Elaborar datos para DataTable
+    data = []
+    for resultado in registros:
+        data.append(
+            {
+                'detalle': {
+                    'nombre': resultado.id,
+                    'url': url_for('exh_exhortos_partes.detail', exh_exhorto_parte_id=resultado.id),
+                },
+                'nombre': resultado.nombre,
+            }
+        )
+    # Entregar JSON
+    return output_datatable_json(draw, total, data)
+
+
+@exh_exhortos_partes.route('/exh_exhortos_partes')
+def list_active():
+    """Listado de Exh Exhortos Partes activos"""
+    return render_template(
+        'exh_exhortos_partes/list.jinja2',
+        filtros=json.dumps({'estatus': 'A'}),
+        titulo='Exhortos Partes',
+        estatus='A',
+    )
+
+
+@exh_exhortos_partes.route('/exh_exhortos_partes/<int:exh_exhorto_parte_id>')
+def detail(exh_exhorto_parte_id):
+    """ Detalle de un Exh Exhorto Parte """
+    exh_exhorto_parte = ExhExhortoParte.query.get_or_404(exh_exhorto_parte_id)
+    return render_template('exh_exhortos_partes/detail.jinja2', exh_exhorto_parte=exh_exhorto_parte)
+
