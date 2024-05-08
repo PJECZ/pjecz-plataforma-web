ac584ba0c877852e7fa1110d49a883d01cbd9b6e
RicVal <6439738+ricval@users.noreply.github.com>
2024-05-08 12:52:51 -0600

    eliminación de lazy

--- plataforma_web/blueprints/exh_areas/models.py
+++ plataforma_web/blueprints/exh_areas/models.py
@@ -1,15 +1,16 @@
 """
 Exh Areas, modelos
 """
+
 from plataforma_web.extensions import db
 from lib.universal_mixin import UniversalMixin
 
 
 class ExhArea(db.Model, UniversalMixin):
-    """ Area """
+    """Area"""
 
     # Nombre de la tabla
-    __tablename__ = 'exh_areas'
+    __tablename__ = "exh_areas"
 
     # Clave primaria
     id = db.Column(db.Integer, primary_key=True)
@@ -19,9 +20,8 @@ class ExhArea(db.Model, UniversalMixin):
     nombre = db.Column(db.String(128), unique=True, nullable=False)
 
     # Hijos
-    exh_exhortos_archivo = db.relationship('ExhExhortoArchivo', back_populates='exh_area', lazy='noload')
-    
+    exh_exhortos_archivo = db.relationship("ExhExhortoArchivo", back_populates="exh_area")
 
     def __repr__(self):
-        """ Representación """
-        return '<ExhArea> {self.id}'
+        """Representación"""
+        return "<ExhArea> {self.id}"
